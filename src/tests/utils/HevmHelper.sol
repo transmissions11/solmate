63a1665c5c3a2bad2909bc1a6820da24281fc63f
t11s <greenfeatherhelp@gmail.com>
2021-07-21 17:17:46 -0700

    ♻️ HevmUser -> HevmHelper

--- src/tests/utils/HevmHelper.sol
+++ src/tests/utils/HevmHelper.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.7.0;
 
 import {Hevm} from "./Hevm.sol";
 
-abstract contract HevmUser {
+abstract contract HevmHelper {
     bytes20 internal constant CHEAT_CODE = bytes20(uint160(uint256(keccak256("hevm cheat code"))));
 
     Hevm internal constant hevm = Hevm(address(CHEAT_CODE));
