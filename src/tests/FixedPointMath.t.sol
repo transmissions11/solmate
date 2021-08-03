b324360a3d2c5f9cd37ee5cf525f1fdea06e873f
t11s <greenfeatherhelp@gmail.com>
2021-08-03 10:03:57 -0700

    ♻️ Test cleanup

--- src/tests/FixedPointMath.t.sol
+++ src/tests/FixedPointMath.t.sol
@@ -6,6 +6,36 @@ import {DSTest} from "ds-test/test.sol";
 import {FixedPointMath} from "../utils/FixedPointMath.sol";
 
 contract FixedPointMathTest is DSTest {
+    function testFMul() public {
+        assertEq(FixedPointMath.fmul(FixedPointMath.RAY, 2.5e27, 0.5e27), 1.25e27);
+        assertEq(FixedPointMath.fmul(FixedPointMath.WAD, 2.5e18, 0.5e18), 1.25e18);
+        assertEq(FixedPointMath.fmul(FixedPointMath.YAD, 2.5e8, 0.5e8), 1.25e8);
+    }
+
+    function testFDiv() public {
+        assertEq(FixedPointMath.fdiv(FixedPointMath.RAY, 1e27, 2e27), 0.5e27);
+        assertEq(FixedPointMath.fdiv(FixedPointMath.WAD, 1e18, 2e18), 0.5e18);
+        assertEq(FixedPointMath.fdiv(FixedPointMath.YAD, 1e8, 2e8), 0.5e8);
+    }
+
+    function testFPow() public {
+        assertEq(FixedPointMath.fpow(FixedPointMath.RAY, 2e27, 2), 4e27);
+        assertEq(FixedPointMath.fpow(FixedPointMath.WAD, 2e18, 2), 4e18);
+        assertEq(FixedPointMath.fpow(FixedPointMath.YAD, 2e8, 2), 4e8);
+    }
+
+    function testSqrt(uint256 x) public {
+        uint256 root = FixedPointMath.sqrt(x);
+        uint256 next = root + 1;
+
+        // Ignore cases where next * next overflows.
+        unchecked {
+            if (next * next < next) return;
+        }
+
+        assertTrue(root * root <= x && next * next > x);
+    }
+
     function testMin(uint256 x, uint256 y) public {
         if (x <= y) {
             assertEq(FixedPointMath.min(x, y), x);
@@ -36,41 +66,5 @@ contract FixedPointMathTest is DSTest {
         } else {
             assertEq(FixedPointMath.imax(x, y), y);
         }
-    }
-
-    function testSqrt(uint256 x) public {
-        uint256 root = FixedPointMath.sqrt(x);
-        uint256 next = root + 1;
-
-        // Ignore cases where next * next overflows.
-        unchecked {
-            if (next * next < next) return;
-        }
-
-        assertTrue(root * root <= x && next * next > x);
-    }
-
-    function testFMul() public {
-        assertEq(FixedPointMath.fmul(FixedPointMath.RAY, 2.5e27, 0.5e27), 1.25e27);
-        assertEq(FixedPointMath.fmul(FixedPointMath.WAD, 2.5e18, 0.5e18), 1.25e18);
-        assertEq(FixedPointMath.fmul(FixedPointMath.YAD, 2.5e8, 0.5e8), 1.25e8);
-    }
-
-    function testFDiv() public {
-        assertEq(FixedPointMath.fdiv(FixedPointMath.RAY, 1e27, 2e27), 0.5e27);
-        assertEq(FixedPointMath.fdiv(FixedPointMath.WAD, 1e18, 2e18), 0.5e18);
-        assertEq(FixedPointMath.fdiv(FixedPointMath.YAD, 1e8, 2e8), 0.5e8);
-    }
-
-    function testSqrt() public {
-        assertEq(FixedPointMath.sqrt(9), 3);
-        assertEq(FixedPointMath.sqrt(16), 4);
-        assertEq(FixedPointMath.sqrt(100), 10);
-    }
-
-    function testFPow() public {
-        assertEq(FixedPointMath.fpow(FixedPointMath.RAY, 2e27, 2), 4e27);
-        assertEq(FixedPointMath.fpow(FixedPointMath.WAD, 2e18, 2), 4e18);
-        assertEq(FixedPointMath.fpow(FixedPointMath.YAD, 2e8, 2), 4e8);
     }
 }
