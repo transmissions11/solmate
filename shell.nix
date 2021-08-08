17aac0e7f98428dfbec9bfd513e61395e6b580c8
t11s <greenfeatherhelp@gmail.com>
2021-08-08 12:02:53 -0700

    👷‍♂️ Update dapptools

--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@ let
   pkgs = import (builtins.fetchGit rec {
     name = "dapptools-${rev}";
     url = https://github.com/dapphub/dapptools;
-    rev = "a2b96cc2dbc28508c9fe699d6438bf6eccafc2ad";
+    rev = "2b2c2a010f1a22a664cf17861c0d6812077f02b7";
   }) {};
 
 in
