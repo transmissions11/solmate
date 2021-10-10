07af469b6d3c61da549dd0f0a4fe6577b4c7b00e
t11s <greenfeatherhelp@gmail.com>
2021-10-10 08:19:30 -0700

    👷‍♂️ Support 0.7.0 in Bytes32AddressLib

--- src/utils/Bytes32AddressLib.sol
+++ src/utils/Bytes32AddressLib.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity 0.8.6;
+pragma solidity >=0.7.0;
 
 /// @notice Library for converting between addresses and bytes32 values.
 /// @author Original work by Transmissions11 (https://github.com/transmissions11)
