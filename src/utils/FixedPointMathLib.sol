0550b962bda897ab0ffe0a0a6927d32a43d092e2
t11s <greenfeatherhelp@gmail.com>
2021-10-21 03:38:55 -0700

    ⚡️ Optimize sqrt

--- src/utils/FixedPointMathLib.sol
+++ src/utils/FixedPointMathLib.sol
@@ -115,56 +115,57 @@ library FixedPointMathLib {
                         GENERAL NUMBER UTILITIES
     //////////////////////////////////////////////////////////////*/
 
-    function sqrt(uint256 x) internal pure returns (uint256 z) {
+    function sqrt(uint256 x) internal pure returns (uint256 result) {
         if (x == 0) return 0;
 
-        uint256 xx = x;
-        uint256 r = 1;
+        result = 1;
 
-        if (xx >= 0x100000000000000000000000000000000) {
-            xx >>= 128;
-            r <<= 64;
+        uint256 xAux = x;
+
+        if (xAux >= 0x100000000000000000000000000000000) {
+            xAux >>= 128;
+            result <<= 64;
         }
 
-        if (xx >= 0x10000000000000000) {
-            xx >>= 64;
-            r <<= 32;
+        if (xAux >= 0x10000000000000000) {
+            xAux >>= 64;
+            result <<= 32;
         }
 
-        if (xx >= 0x100000000) {
-            xx >>= 32;
-            r <<= 16;
+        if (xAux >= 0x100000000) {
+            xAux >>= 32;
+            result <<= 16;
         }
 
-        if (xx >= 0x10000) {
-            xx >>= 16;
-            r <<= 8;
+        if (xAux >= 0x10000) {
+            xAux >>= 16;
+            result <<= 8;
         }
 
-        if (xx >= 0x100) {
-            xx >>= 8;
-            r <<= 4;
+        if (xAux >= 0x100) {
+            xAux >>= 8;
+            result <<= 4;
         }
 
-        if (xx >= 0x10) {
-            xx >>= 4;
-            r <<= 2;
+        if (xAux >= 0x10) {
+            xAux >>= 4;
+            result <<= 2;
         }
 
-        if (xx >= 0x8) r <<= 1;
+        if (xAux >= 0x8) result <<= 1;
 
         unchecked {
-            r = (r + x / r) >> 1;
-            r = (r + x / r) >> 1;
-            r = (r + x / r) >> 1;
-            r = (r + x / r) >> 1;
-            r = (r + x / r) >> 1;
-            r = (r + x / r) >> 1;
-            r = (r + x / r) >> 1;
+            result = (result + x / result) >> 1;
+            result = (result + x / result) >> 1;
+            result = (result + x / result) >> 1;
+            result = (result + x / result) >> 1;
+            result = (result + x / result) >> 1;
+            result = (result + x / result) >> 1;
+            result = (result + x / result) >> 1;
 
-            uint256 r1 = x / r;
+            uint256 roundedDownResult = x / result;
 
-            return r < r1 ? r : r1;
+            if (result >= roundedDownResult) result = roundedDownResult;
         }
     }
 
