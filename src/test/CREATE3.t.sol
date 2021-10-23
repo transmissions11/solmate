34da6946139e2c0ca8c401aeb5c8b4eb533a42e6
t11s <greenfeatherhelp@gmail.com>
2021-10-22 22:02:33 -0700

    ✅ Test SSTORE2

---
+++ src/test/CREATE3.t.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity 0.8.9;
+
+import {DSTestPlus} from "./utils/DSTestPlus.sol";
+
+import {CREATE3} from "../utils/CREATE3.sol";
+
+contract CREATE3Test is DSTestPlus {}
