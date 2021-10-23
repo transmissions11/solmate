93a1709b1b9b127ddb11863a42e6170533216972
t11s <greenfeatherhelp@gmail.com>
2021-10-23 00:32:03 -0700

    ✅ Create3 Tests

--- src/test/CREATE3.t.sol
+++ src/test/CREATE3.t.sol
@@ -2,7 +2,44 @@
 pragma solidity 0.8.9;
 
 import {DSTestPlus} from "./utils/DSTestPlus.sol";
+import {MockERC20} from "./utils/mocks/MockERC20.sol";
 
 import {CREATE3} from "../utils/CREATE3.sol";
 
-contract CREATE3Test is DSTestPlus {}
+contract MinimalERC20 is MockERC20("Minimal ERC20", "ERC20", 18) {}
+
+contract CREATE3Test is DSTestPlus {
+    function testDeployERC20() public {
+        bytes32 salt = keccak256(bytes("A salt!"));
+
+        MinimalERC20 deployed = MinimalERC20(CREATE3.deploy(salt, type(MinimalERC20).creationCode));
+
+        assertEq(address(deployed), CREATE3.getDeployed(salt));
+
+        assertEq(deployed.name(), "Minimal ERC20");
+        assertEq(deployed.symbol(), "ERC20");
+        assertEq(deployed.decimals(), 18);
+    }
+
+    function testFailDoubleDeployERC20() public {
+        bytes32 salt = keccak256(bytes("A salt!"));
+
+        CREATE3.deploy(salt, type(MinimalERC20).creationCode);
+        CREATE3.deploy(salt, type(MinimalERC20).creationCode);
+    }
+
+    function testDeployERC20(bytes32 salt) public {
+        MinimalERC20 deployed = MinimalERC20(CREATE3.deploy(salt, type(MinimalERC20).creationCode));
+
+        assertEq(address(deployed), CREATE3.getDeployed(salt));
+
+        assertEq(deployed.name(), "Minimal ERC20");
+        assertEq(deployed.symbol(), "ERC20");
+        assertEq(deployed.decimals(), 18);
+    }
+
+    function testFailDoubleDeployERC20(bytes32 salt) public {
+        CREATE3.deploy(salt, type(MinimalERC20).creationCode);
+        CREATE3.deploy(salt, type(MinimalERC20).creationCode);
+    }
+}
