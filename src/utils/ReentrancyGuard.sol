5805da4db0eb25c76f164056e0c091622494054c
t11s <greenfeatherhelp@gmail.com>
2021-10-30 18:46:16 -0700

    ⚡️ Optimize ReentrancyGuard

--- src/utils/ReentrancyGuard.sol
+++ src/utils/ReentrancyGuard.sol
@@ -4,18 +4,15 @@ pragma solidity >=0.7.0;
 /// @notice Gas optimized reentrancy protection for smart contracts.
 /// @author Modified from OpenZeppelin (https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/security/ReentrancyGuard.sol)
 abstract contract ReentrancyGuard {
-    uint256 private constant NOT_ENTERED = 1;
-    uint256 private constant ENTERED = 2;
-
-    uint256 private reentrancyStatus = NOT_ENTERED;
+    uint256 private reentrancyStatus = 1;
 
     modifier nonReentrant() {
-        require(reentrancyStatus != ENTERED, "REENTRANCY");
+        require(reentrancyStatus == 1, "REENTRANCY");
 
-        reentrancyStatus = ENTERED;
+        reentrancyStatus = 2;
 
         _;
 
-        reentrancyStatus = NOT_ENTERED;
+        reentrancyStatus = 1;
     }
 }
