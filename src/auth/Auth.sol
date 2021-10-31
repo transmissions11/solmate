293c282cff974756224847be2c32169e07925986
t11s <greenfeatherhelp@gmail.com>
2021-10-30 22:03:08 -0700

    🚧 Attempt to hyper optimize auth

--- src/auth/Auth.sol
+++ src/auth/Auth.sol
@@ -42,14 +42,47 @@ abstract contract Auth {
         emit AuthorityUpdated(authority);
     }
 
-    function isAuthorized(address user, bytes4 functionSig) internal view virtual returns (bool) {
-        Authority cachedAuthority = authority;
+    function isAuthorized(address user, bytes4 functionSig) internal view virtual returns (bool authorized) {
+        assembly {
+            let cachedAuthority := sload(authority.slot)
 
-        if (address(cachedAuthority) != address(0) && cachedAuthority.canCall(user, address(this), functionSig)) {
-            return true;
+            if iszero(eq(cachedAuthority, 0x0000000000000000000000000000000000000000000000000000000000000000)) {
+                // Get a pointer to some memory.
+                let callData := mload(0x40)
+
+                // Write the abi-encoded calldata to the slot in memory piece by piece:
+                mstore(callData, shl(224, 0xb7009613)) // Begin with the function selector.
+                mstore(add(callData, 4), and(user, 0xffffffffffffffffffffffffffffffffffffffff)) // Mask and append the "user" argument.
+                mstore(add(callData, 36), and(address(), 0xffffffffffffffffffffffffffffffffffffffff)) // Mask and append our address.
+                mstore(
+                    add(callData, 68),
+                    and(functionSig, 0xffffffff00000000000000000000000000000000000000000000000000000000)
+                ) // Finally append the "functionSig" argument. Must be masked as a bytes4 value.
+
+                // Call the authority and store if it succeeded or not.
+                // We use 100 because the calldata length is 4 + 32 * 3.
+                let callStatus := staticcall(gas(), cachedAuthority, 0, 100, 0, 0)
+
+                // Get how many bytes the call returned.
+                let returnDataSize := returndatasize()
+
+                // Copy the return data into memory.
+                returndatacopy(0, 0, returnDataSize)
+
+                // If the call reverted:
+                if iszero(callStatus) {
+                    // Revert with the same message .
+                    revert(0, returnDataSize)
+                }
+
+                // Set authorized to whether it returned true.
+                authorized := iszero(iszero(mload(0)))
+            }
+
+            if iszero(authorized) {
+                authorized := eq(sload(owner.slot), and(user, 0xffffffffffffffffffffffffffffffffffffffff))
+            }
         }
-
-        return user == owner;
     }
 
     modifier requiresAuth() {
