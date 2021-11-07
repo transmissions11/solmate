54e3aa8cd8bcf5ec67a687ac2fa7af99536d3f21
t11s <greenfeatherhelp@gmail.com>
2021-11-06 23:39:56 -0700

    ✨ SafeCastLib

---
+++ src/test/SafeCastLib.t.sol
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity 0.8.9;
+
+import {DSTestPlus} from "./utils/DSTestPlus.sol";
+import {SafeCastLib} from "../utils/SafeCastLib.sol";
+
+contract SafeCastLibTest is DSTestPlus {
+    function testSafeCastTo224() public {
+        assertEq(SafeCastLib.safeCastTo224(2.5e45), 2.5e45);
+        assertEq(SafeCastLib.safeCastTo224(2.5e27), 2.5e27);
+    }
+
+    function testSafeCastTo128() public {
+        assertEq(SafeCastLib.safeCastTo128(2.5e27), 2.5e27);
+        assertEq(SafeCastLib.safeCastTo128(2.5e18), 2.5e18);
+    }
+
+    function testSafeCastTo64() public {
+        assertEq(SafeCastLib.safeCastTo64(2.5e18), 2.5e18);
+        assertEq(SafeCastLib.safeCastTo64(2.5e17), 2.5e17);
+    }
+
+    function testFailSafeCastTo224() public {
+        SafeCastLib.safeCastTo224(type(uint224).max + 1);
+    }
+
+    function testFailSafeCastTo128() public {
+        SafeCastLib.safeCastTo128(type(uint128).max + 1);
+    }
+
+    function testFailSafeCastTo64() public {
+        SafeCastLib.safeCastTo64(type(uint64).max + 1);
+    }
+
+    //
+
+    function testSafeCastTo224(uint256 x) public {
+        x %= type(uint224).max;
+
+        assertEq(SafeCastLib.safeCastTo224(x), x);
+    }
+
+    function testSafeCastTo128(uint256 x) public {
+        x %= type(uint128).max;
+
+        assertEq(SafeCastLib.safeCastTo128(x), x);
+    }
+
+    function testSafeCastTo64(uint256 x) public {
+        x %= type(uint64).max;
+
+        assertEq(SafeCastLib.safeCastTo64(x), x);
+    }
+
+    function testFailSafeCastTo224(uint256 x) public {
+        if (type(uint224).max > x) revert();
+
+        SafeCastLib.safeCastTo224(x);
+    }
+
+    function testFailSafeCastTo128(uint256 x) public {
+        if (type(uint128).max > x) revert();
+
+        SafeCastLib.safeCastTo128(x);
+    }
+
+    function testFailSafeCastTo64(uint256 x) public {
+        if (type(uint64).max > x) revert();
+
+        SafeCastLib.safeCastTo64(x);
+    }
+}
---
+++ src/utils/SafeCastLib.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity >=0.8.0;
+
+/// @notice Safe unsigned integer casting library that reverts on overflow.
+/// @author Modified from OpenZeppelin (https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeCast.sol)
+library SafeCastLib {
+    function safeCastTo224(uint256 x) internal pure returns (uint224 y) {
+        require(x <= type(uint224).max);
+
+        y = uint224(x);
+    }
+
+    function safeCastTo128(uint256 x) internal pure returns (uint128 y) {
+        require(x <= type(uint128).max);
+
+        y = uint128(x);
+    }
+
+    function safeCastTo64(uint256 x) internal pure returns (uint64 y) {
+        require(x <= type(uint64).max);
+
+        y = uint64(x);
+    }
+}
