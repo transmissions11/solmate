17ab906bb9a4275eb2b05440d179b423668dc003
t11s <greenfeatherhelp@gmail.com>
2021-12-01 12:43:04 -0800

    🚧 WIP

--- src/utils/CREATE3.sol
+++ src/utils/CREATE3.sol
@@ -8,6 +8,30 @@ import {Bytes32AddressLib} from "./Bytes32AddressLib.sol";
 library CREATE3 {
     using Bytes32AddressLib for bytes32;
 
+    //   0x00  0x67  0x67XXXXXXXXXXXXXXXX  PUSH8 bytecode  0x363d3d37363d34f0
+    //   0x01  0x3d  0x3d                  RETURNDATASIZE  0 0x363d3d37363d34f0
+    //   0x02  0x52  0x52                  MSTORE
+    //   0x03  0x60  0x6008                PUSH1 08        8
+    //   0x04  0x60  0x6018                PUSH1 18        24 8
+    //   0x05  0xf3  0xf3                  RETURN
+
+    //--------------------------------------------------------------------------------//
+    // Opcode     | Opcode + Arguments  | Description        | Stack View             //
+    //--------------------------------------------------------------------------------//
+    // 0x67       |  0x67XXXXXXXXXXXXXXXX | PUSH8 bytecode   | 0x363d3d37363d34f0     //
+    // 0x3d       |  0x3d                 | RETURNDATASIZE   | 0 0x363d3d37363d34f0   //
+    // 0x52       |  0x52                 | MSTORE           |                        //
+    // 0x60       |  0x6008               | PUSH1 08         | 8                      //
+    // 0x60       |  0x6018               | PUSH1 18         | 24 8                   //
+    // 0xf3       |  0xf3                 | RETURN           |                        //
+    //--------------------------------------------------------------------------------//
+
+    //   0x00  0x67  0x67XXXXXXXXXXXXXXXX  PUSH8 bytecode  0x363d3d37363d34f0
+    //   0x01  0x3d  0x3d                  RETURNDATASIZE  0 0x363d3d37363d34f0
+    //   0x02  0x52  0x52                  MSTORE
+    //   0x03  0x60  0x6008                PUSH1 08        8
+    //   0x04  0x60  0x6018                PUSH1 18        24 8
+    //   0x05  0xf3  0xf3                  RETURN
     bytes internal constant PROXY_BYTECODE = hex"67_36_3d_3d_37_36_3d_34_f0_3d_52_60_08_60_18_f3";
 
     bytes32 internal constant PROXY_BYTECODE_HASH = keccak256(PROXY_BYTECODE);
@@ -15,14 +39,15 @@ library CREATE3 {
     function deploy(bytes32 salt, bytes memory creationCode) internal returns (address deployed) {
         bytes memory proxyChildBytecode = PROXY_BYTECODE;
 
-        deployed = getDeployed(salt);
-
         address proxy;
         assembly {
+            // Deploy a new contract with our pre-made bytecode via CREATE2.
+            // We start 32 bytes into the code to avoid copying the byte length.
             proxy := create2(0, add(proxyChildBytecode, 32), mload(proxyChildBytecode), salt)
         }
         require(proxy != address(0), "DEPLOYMENT_FAILED");
 
+        deployed = getDeployed(salt);
         (bool success, ) = proxy.call(creationCode);
         require(success && deployed.code.length != 0, "INITIALIZATION_FAILED");
     }
--- src/utils/SSTORE2.sol
+++ src/utils/SSTORE2.sol
@@ -1,25 +1,42 @@
 // SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity >=0.7.0;
+pragma solidity >=0.8.0;
 
 /// @notice Read and write to persistent storage at a fraction of the cost.
 /// @author Modified from 0xSequence (https://github.com/0xsequence/sstore2/blob/master/contracts/SSTORE2.sol)
 library SSTORE2 {
-    uint256 internal constant DATA_OFFSET = 1;
+    uint256 internal constant DATA_OFFSET = 1; // We skip the first byte as it's null to ensure the contract can't be called.
 
     function write(bytes memory data) internal returns (address pointer) {
+        // Prefix the bytecode with a zero byte to ensure it cannot be called.
         bytes memory runtimeCode = abi.encodePacked(hex"00", data);
 
+        // Compute the creation code we need from our desired runtime code.
         bytes memory creationCode = abi.encodePacked(
-            hex"63",
-            uint32(runtimeCode.length),
-            hex"80_60_0E_60_00_39_60_00_F3",
-            runtimeCode
+            //--------------------------------------------------------------------------------//
+            // Opcode     | Opcode + Arguments  | Description        | Stack View             //
+            //--------------------------------------------------------------------------------//
+            // 0x63       |  0x63XXXXXX         | PUSH4 codeSize     | codeSize               //
+            // 0x80       |  0x80               | DUP1               | codeSize codeSize      //
+            // 0x60       |  0x600e             | PUSH1 14           | 14 codeSize codeSize   //
+            // 0x60       |  0x6000             | PUSH1 00           | 0 14 codeSize codeSize //
+            // 0x39       |  0x39               | CODECOPY           | codeSize               //
+            // 0x60       |  0x6000             | PUSH1 00           | 0 codeSize             //
+            // 0xf3       |  0xf3               | RETURN             |                        //
+            //--------------------------------------------------------------------------------//
+            hex"63", // We use a 4 byte PUSH for future proofing, but in theory we could use a smaller PUSH.
+            uint32(runtimeCode.length), // We must cast it to a 32 bit number (4 bytes) as we use a PUSH4 above.
+            // If we used a smaller PUSH we'd need to change 0E (14 in hex) as it's used as the size of the creation code.
+            hex"80_60_0E_60_00_39_60_00_F3", // Optimized constructor code, copies the runtime code into memory and returns it.
+            runtimeCode // The bytecode we want the contract to have after deployment. Capped at 1 byte less than the code size limit.
         );
 
         assembly {
+            // Deploy a new contract with the generated creation code.
+            // We start 32 bytes into the code to avoid copying the byte length.
             pointer := create(0, add(creationCode, 32), mload(creationCode))
         }
 
+        // If the pointer is 0 then the deployment failed.
         require(pointer != address(0), "DEPLOYMENT_FAILED");
     }
 
@@ -52,10 +69,17 @@ library SSTORE2 {
         uint256 size
     ) private view returns (bytes memory data) {
         assembly {
+            // Get a pointer to some free memory.
             data := mload(0x40)
-            mstore(0x40, add(data, and(add(add(size, add(start, 0x20)), 0x1f), not(0x1f))))
+
+            // Update the free memory pointer to prevent overriding our data.
+            mstore(0x40, add(data, and(add(size, 31), not(31))))
+
+            // Store the size of the data in the first slot (32 bytes) of free memory.
             mstore(data, size)
-            extcodecopy(pointer, add(data, 0x20), start, size)
+
+            // Copy the code into memory right after the 32 bytes we used to store the size.
+            extcodecopy(pointer, add(data, 32), start, size)
         }
     }
 }
