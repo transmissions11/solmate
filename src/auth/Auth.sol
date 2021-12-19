ae2d320578635cb8ecbdf2801ce6a745a199e413
t11s <greenfeatherhelp@gmail.com>
2021-12-18 21:07:47 -0800

    🔒 Safer Auth.sol

--- src/auth/Auth.sol
+++ src/auth/Auth.sol
@@ -30,33 +30,33 @@ abstract contract Auth {
         emit AuthorityUpdated(_authority);
     }
 
-    function setOwner(address newOwner) public virtual requiresAuth {
-        owner = newOwner;
+    modifier requiresAuth() {
+        require(isAuthorized(msg.sender, msg.sig), "UNAUTHORIZED");
 
-        emit OwnerUpdated(newOwner);
+        _;
     }
 
-    function setAuthority(Authority newAuthority) public virtual requiresAuth {
+    function isAuthorized(address user, bytes4 functionSig) internal view virtual returns (bool) {
+        Authority auth = authority; // Memoizing authority saves us a warm SLOAD, around 100 gas.
+
+        // Checking if the caller is the owner only after calling the authority saves gas in most cases, but
+        // be aware this renders authorized functions uncallable even to the owner if the authority reverts.
+        return (address(auth) != address(0) && auth.canCall(user, address(this), functionSig)) || user == owner;
+    }
+
+    function setAuthority(Authority newAuthority) public virtual {
+        // We check if the caller is the owner first because we want to ensure they can
+        // always swap out the authority even if it's reverting or using a lot of gas.
+        require(msg.sender == owner || authority.canCall(msg.sender, address(this), msg.sig));
+
         authority = newAuthority;
 
         emit AuthorityUpdated(newAuthority);
     }
 
-    function isAuthorized(address user, bytes4 functionSig) internal view virtual returns (bool) {
-        Authority cachedAuthority = authority;
+    function setOwner(address newOwner) public virtual requiresAuth {
+        owner = newOwner;
 
-        if (address(cachedAuthority) != address(0)) {
-            try cachedAuthority.canCall(user, address(this), functionSig) returns (bool canCall) {
-                if (canCall) return true;
-            } catch {}
-        }
-
-        return user == owner;
-    }
-
-    modifier requiresAuth() {
-        require(isAuthorized(msg.sender, msg.sig), "UNAUTHORIZED");
-
-        _;
+        emit OwnerUpdated(newOwner);
     }
 }
