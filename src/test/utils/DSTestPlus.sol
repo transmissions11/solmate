30505227a36134a2fed60f769a5f7aab091ddfa4
t11s <greenfeatherhelp@gmail.com>
2021-12-19 20:56:20 -0800

    🔒 Safety check on bound

--- src/test/utils/DSTestPlus.sol
+++ src/test/utils/DSTestPlus.sol
@@ -104,6 +104,8 @@ contract DSTestPlus is DSTest {
         uint256 min,
         uint256 max
     ) internal pure returns (uint256) {
+        require(max > min, "MAX_NOT_GREATER_THAN_MIN");
+
         x = max == type(uint256).max && x != 0 ? x - 1 : x;
         max = max == type(uint256).max ? max : max + 1;
 
