edefb3be4dd74ff4cb8cacf90fbebf694137894f
t11s <greenfeatherhelp@gmail.com>
2021-12-24 14:41:48 -0800

    ♻️ x2 -> y

--- src/utils/FixedPointMathLib.sol
+++ src/utils/FixedPointMathLib.sol
@@ -153,34 +153,34 @@ library FixedPointMathLib {
             z := 1
 
             // Used below to help find a nearby power of 2.
-            let x2 := x
+            let y := x
 
             // Find the lowest power of 2 that is at least sqrt(x).
-            if iszero(lt(x2, 0x100000000000000000000000000000000)) {
-                x2 := shr(128, x2) // Like dividing by 2 ** 128.
+            if iszero(lt(y, 0x100000000000000000000000000000000)) {
+                y := shr(128, y) // Like dividing by 2 ** 128.
                 z := shl(64, z)
             }
-            if iszero(lt(x2, 0x10000000000000000)) {
-                x2 := shr(64, x2) // Like dividing by 2 ** 64.
+            if iszero(lt(y, 0x10000000000000000)) {
+                y := shr(64, y) // Like dividing by 2 ** 64.
                 z := shl(32, z)
             }
-            if iszero(lt(x2, 0x100000000)) {
-                x2 := shr(32, x2) // Like dividing by 2 ** 32.
+            if iszero(lt(y, 0x100000000)) {
+                y := shr(32, y) // Like dividing by 2 ** 32.
                 z := shl(16, z)
             }
-            if iszero(lt(x2, 0x10000)) {
-                x2 := shr(16, x2) // Like dividing by 2 ** 16.
+            if iszero(lt(y, 0x10000)) {
+                y := shr(16, y) // Like dividing by 2 ** 16.
                 z := shl(8, z)
             }
-            if iszero(lt(x2, 0x100)) {
-                x2 := shr(8, x2) // Like dividing by 2 ** 8.
+            if iszero(lt(y, 0x100)) {
+                y := shr(8, y) // Like dividing by 2 ** 8.
                 z := shl(4, z)
             }
-            if iszero(lt(x2, 0x10)) {
-                x2 := shr(4, x2) // Like dividing by 2 ** 4.
+            if iszero(lt(y, 0x10)) {
+                y := shr(4, y) // Like dividing by 2 ** 4.
                 z := shl(2, z)
             }
-            if iszero(lt(x2, 0x8)) {
+            if iszero(lt(y, 0x8)) {
                 // Equivalent to 2 ** z.
                 z := shl(1, z)
             }
