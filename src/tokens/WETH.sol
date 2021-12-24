3efc9a095c3f24e5187c10c66984149b45ae63d6
t11s <greenfeatherhelp@gmail.com>
2021-12-23 23:05:31 -0800

    ♻️ Public virtual

--- src/tokens/WETH.sol
+++ src/tokens/WETH.sol
@@ -15,13 +15,13 @@ contract WETH is ERC20("Wrapped Ether", "WETH", 18) {
 
     event Withdrawal(address indexed to, uint256 amount);
 
-    function deposit() public payable {
+    function deposit() public payable virtual {
         _mint(msg.sender, msg.value);
 
         emit Deposit(msg.sender, msg.value);
     }
 
-    function withdraw(uint256 amount) external {
+    function withdraw(uint256 amount) public virtual {
         _burn(msg.sender, amount);
 
         emit Withdrawal(msg.sender, amount);
@@ -29,7 +29,7 @@ contract WETH is ERC20("Wrapped Ether", "WETH", 18) {
         msg.sender.safeTransferETH(amount);
     }
 
-    receive() external payable {
+    receive() external payable virtual {
         deposit();
     }
 }
