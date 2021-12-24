51bfefc6b3b37b7ca807a460aabb9c7685d403ef
t11s <greenfeatherhelp@gmail.com>
2021-12-24 12:13:56 -0800

    ✅ More sqrt tests

--- src/test/FixedPointMathLib.t.sol
+++ src/test/FixedPointMathLib.t.sol
@@ -49,6 +49,7 @@ contract FixedPointMathLibTest is DSTestPlus {
     }
 
     function testSqrt() public {
+        assertEq(FixedPointMathLib.sqrt(1), 1);
         assertEq(FixedPointMathLib.sqrt(2704), 52);
         assertEq(FixedPointMathLib.sqrt(110889), 333);
         assertEq(FixedPointMathLib.sqrt(32239684), 5678);
