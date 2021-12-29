ba588f3632c9a931241be2b6f33813b832446d6b
t11s <greenfeatherhelp@gmail.com>
2021-12-28 21:43:20 -0800

    ⚡️ Optimize computeDomainSeparator

--- src/tokens/ERC20.sol
+++ src/tokens/ERC20.sol
@@ -158,7 +158,7 @@ abstract contract ERC20 {
                 abi.encode(
                     keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
                     keccak256(bytes(name)),
-                    keccak256(bytes("1")),
+                    keccak256("1"),
                     block.chainid,
                     address(this)
                 )
