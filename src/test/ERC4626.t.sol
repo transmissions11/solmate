7f4ec39baf43bb9f51402e848aedb0efc0277dae
zerosnacks <zerosnacks@protonmail.com>
2022-01-13 11:50:18 +0100

    let ERC4626User inherit ERC20User and implement case for multiple users depositing into the vault

--- src/test/ERC4626.t.sol
+++ src/test/ERC4626.t.sol
@@ -33,7 +33,7 @@ contract ERC4626Test is DSTestPlus {
                         DEPOSIT/WITHDRAWAL TESTS
     //////////////////////////////////////////////////////////////*/
 
-    function testAtomicDepositWithdraw() public {
+    function testSingleAtomicDepositWithdraw() public {
         // uint256 amount = fuzzAmount / 10**underlying.decimals();
         uint256 amount = 2e18;
 
@@ -57,29 +57,68 @@ contract ERC4626Test is DSTestPlus {
         assertEq(underlying.balanceOf(address(this)), preDepositBal);
     }
 
-    function testAtomicDepositRedeem() public {
-        // uint256 amount = fuzzAmount / 10**underlying.decimals();
-        uint256 amount = 2e18;
+    // dapp test -m ':ERC4626Test\.testMultipleAtomicDepositWithdraw' --verbosity 3
+    function testMultipleAtomicDepositWithdraw() public {
+        ERC4626User alice = new ERC4626User(vault, underlying);
+        ERC4626User bob = new ERC4626User(vault, underlying);
 
-        underlying.mint(address(this), amount);
-        underlying.approve(address(vault), amount);
+        uint256 aliceAmount = 2e18;
+        uint256 bobAmount = 3e18;
 
-        uint256 preDepositBal = underlying.balanceOf(address(this));
+        underlying.mint(address(alice), aliceAmount);
+        alice.approve(address(vault), aliceAmount);
 
-        vault.deposit(address(this), amount);
+        underlying.mint(address(bob), bobAmount);
+        bob.approve(address(vault), bobAmount);
 
-        assertEq(vault.totalHoldings(), amount);
-        assertEq(vault.balanceOf(address(this)), amount);
-        assertEq(vault.balanceOfUnderlying(address(this)), amount);
-        assertEq(underlying.balanceOf(address(this)), preDepositBal - amount);
+        uint256 alicePreDepositBal = underlying.balanceOf(address(alice));
+        uint256 bobPreDepositBal = underlying.balanceOf(address(bob));
 
-        vault.redeem(address(this), address(this), amount);
+        assertEq(alicePreDepositBal, aliceAmount);
+        assertEq(bobPreDepositBal, bobAmount);
+        assertEq(underlying.allowance(address(alice), address(vault)), aliceAmount);
+        assertEq(underlying.allowance(address(bob), address(vault)), bobAmount);
 
-        assertEq(vault.totalHoldings(), 0);
-        assertEq(vault.balanceOf(address(this)), 0);
-        assertEq(vault.balanceOfUnderlying(address(this)), 0);
-        assertEq(underlying.balanceOf(address(this)), preDepositBal);
+        uint256 aliceShares = alice.deposit(address(alice), aliceAmount);
+        assertEq(aliceShares, aliceAmount);
+
+        assertEq(vault.totalHoldings(), aliceAmount);
+        assertEq(vault.balanceOf(address(alice)), aliceAmount);
+        assertEq(vault.balanceOfUnderlying(address(alice)), aliceAmount);
+        assertEq(underlying.balanceOf(address(alice)), alicePreDepositBal - aliceAmount);
+
+        uint256 bobShares = bob.deposit(address(bob), bobAmount);
+        assertEq(bobShares, bobAmount);
+
+        assertEq(vault.totalHoldings(), aliceAmount + bobAmount);
+        assertEq(vault.balanceOf(address(bob)), bobAmount);
+        assertEq(vault.balanceOfUnderlying(address(bob)), bobAmount);
+        assertEq(underlying.balanceOf(address(bob)), bobPreDepositBal - bobAmount);
     }
+
+    // function testSingleAtomicDepositRedeem() public {
+    //     // uint256 amount = fuzzAmount / 10**underlying.decimals();
+    //     uint256 amount = 2e18;
+
+    //     underlying.mint(address(this), amount);
+    //     underlying.approve(address(vault), amount);
+
+    //     uint256 preDepositBal = underlying.balanceOf(address(this));
+
+    //     vault.deposit(address(this), amount);
+
+    //     assertEq(vault.totalHoldings(), amount);
+    //     assertEq(vault.balanceOf(address(this)), amount);
+    //     assertEq(vault.balanceOfUnderlying(address(this)), amount);
+    //     assertEq(underlying.balanceOf(address(this)), preDepositBal - amount);
+
+    //     vault.redeem(address(this), address(this), amount);
+
+    //     assertEq(vault.totalHoldings(), 0);
+    //     assertEq(vault.balanceOf(address(this)), 0);
+    //     assertEq(vault.balanceOfUnderlying(address(this)), 0);
+    //     assertEq(underlying.balanceOf(address(this)), preDepositBal);
+    // }
 
     /*///////////////////////////////////////////////////////////////
                  DEPOSIT/WITHDRAWAL SANITY CHECK TESTS
@@ -88,6 +127,7 @@ contract ERC4626Test is DSTestPlus {
     function testFailDepositWithNotEnoughApproval() public {
         underlying.mint(address(this), 0.5e18);
         underlying.approve(address(vault), 0.5e18);
+        assertEq(underlying.allowance(address(this), address(vault)), 0.5e18);
 
         vault.deposit(address(this), 1e18);
     }
--- src/test/utils/users/ERC4626User.sol
+++ src/test/utils/users/ERC4626User.sol
@@ -4,20 +4,16 @@ pragma solidity >=0.8.0;
 import {ERC20} from "../../../tokens/ERC20.sol";
 import {ERC4626} from "../../../mixins/ERC4626.sol";
 
-contract ERC4626User {
-    ERC4626 token;
-    ERC20 underlying;
+import {ERC20User} from "./ERC20User.sol";
 
-    constructor(ERC4626 _token, ERC20 _underlying) {
-        token = _token;
-        underlying = _underlying;
-    }
+contract ERC4626User is ERC20User {
+    ERC4626 vault;
 
-    function approve(address spender, uint256 amount) public virtual returns (bool) {
-        return token.approve(spender, amount);
+    constructor(ERC4626 _vault, ERC20 _token) ERC20User(_token) {
+        vault = _vault;
     }
 
     function deposit(address to, uint256 underlyingAmount) public virtual returns (uint256 shares) {
-        return token.deposit(to, underlyingAmount);
+        return vault.deposit(to, underlyingAmount);
     }
 }
