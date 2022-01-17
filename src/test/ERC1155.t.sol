35aa940719b9f1c975505d0b739f3d7702f373bb
t11s <greenfeatherhelp@gmail.com>
2022-01-17 00:24:26 -0800

    ✅ Fix ERC1155 fail tests

--- src/test/ERC1155.t.sol
+++ src/test/ERC1155.t.sol
@@ -1371,9 +1371,9 @@ contract ERC1155Test is DSTestPlus, ERC1155TokenReceiver {
     ) public {
         ERC1155User from = new ERC1155User(token);
 
-        if (ids.length == 0 && mintAmounts.length == 0 && transferAmounts.length == 0) revert();
-
         uint256 minLength = min3(ids.length, mintAmounts.length, transferAmounts.length);
+
+        if (minLength == 0) revert();
 
         uint256[] memory normalizedIds = new uint256[](minLength);
         uint256[] memory normalizedMintAmounts = new uint256[](minLength);
@@ -1714,9 +1714,9 @@ contract ERC1155Test is DSTestPlus, ERC1155TokenReceiver {
         uint256[] memory burnAmounts,
         bytes memory mintData
     ) public {
-        if (ids.length == 0 && mintAmounts.length == 0 && burnAmounts.length == 0) revert();
-
         uint256 minLength = min3(ids.length, mintAmounts.length, burnAmounts.length);
+
+        if (minLength == 0) revert();
 
         uint256[] memory normalizedIds = new uint256[](minLength);
         uint256[] memory normalizedMintAmounts = new uint256[](minLength);
