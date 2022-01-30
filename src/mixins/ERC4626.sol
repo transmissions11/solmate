d22d831c1ba559cfd47f54c3d8095da898aaefdd
t11s <greenfeatherhelp@gmail.com>
2022-01-30 12:49:56 -0800

    ♻️ Unlock pragma

--- src/mixins/ERC4626.sol
+++ src/mixins/ERC4626.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity 0.8.10;
+pragma solidity >=0.8.0;
 
 import {ERC20} from "../tokens/ERC20.sol";
 import {SafeTransferLib} from "../utils/SafeTransferLib.sol";
