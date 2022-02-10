c3b84d0604e9a01430702b5793737ef9577c942e
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2022-02-10 11:19:42 +0100

    ensure inlining

--- src/tokens/ERC1155.sol
+++ src/tokens/ERC1155.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity >=0.8.0;
+pragma solidity >=0.8.2;
 
 /// @notice Minimalist and gas efficient standard ERC1155 implementation.
 /// @author Solmate (https://github.com/Rari-Capital/solmate/blob/main/src/tokens/ERC1155.sol)
