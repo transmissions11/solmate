1205a9067ff957ef8b0b003ff9d77c20ef9f2e0b
Joey <31974730+Joeysantoro@users.noreply.github.com>
2022-02-10 22:27:59 -0800

    ✨ Add Foundry cheatcodes to Hevm.sol (#127)
    Co-authored-by: t11s <greenfeatherhelp@gmail.com>

--- src/test/utils/Hevm.sol
+++ src/test/utils/Hevm.sol
@@ -6,13 +6,15 @@ interface Hevm {
 
     function roll(uint256) external;
 
+    function fee(uint256) external;
+
+    function load(address, bytes32) external returns (bytes32);
+
     function store(
         address,
         bytes32,
         bytes32
     ) external;
-
-    function load(address, bytes32) external returns (bytes32);
 
     function sign(uint256, bytes32)
         external
@@ -25,4 +27,45 @@ interface Hevm {
     function addr(uint256) external returns (address);
 
     function ffi(string[] calldata) external returns (bytes memory);
+
+    function prank(address) external;
+
+    function startPrank(address) external;
+
+    function prank(address, address) external;
+
+    function startPrank(address, address) external;
+
+    function stopPrank() external;
+
+    function deal(address, uint256) external;
+
+    function etch(address, bytes calldata) external;
+
+    function expectRevert(bytes calldata) external;
+
+    function expectRevert(bytes4) external;
+
+    function record() external;
+
+    function accesses(address) external returns (bytes32[] memory reads, bytes32[] memory writes);
+
+    function expectEmit(
+        bool,
+        bool,
+        bool,
+        bool
+    ) external;
+
+    function mockCall(
+        address,
+        bytes calldata,
+        bytes calldata
+    ) external;
+
+    function clearMockedCalls() external;
+
+    function expectCall(address, bytes calldata) external;
+
+    function getCode(string calldata) external returns (bytes memory);
 }
