b3f58febcba6cf40bae67a5776c8c37a7780b502
Omar <omar@na2.com.br>
2022-02-22 12:05:58 +0000

    updated eip712 to library

--- src/utils/EIP712.sol
+++ src/utils/EIP712.sol
@@ -5,48 +5,31 @@ pragma solidity >=0.8.0;
 /// @author Solmate (https://github.com/Rari-Capital/solmate/blob/main/src/utils/EIP712Lib.sol)
 /// @author Inspired by 0xProject (https://github.com/0xProject/0x-monorepo/blob/development/contracts/utils/contracts/src/LibEIP712.sol)
 /// @author Inspired by OpenZeppelin (https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/cryptography/draft-EIP712.sol)
-abstract contract EIP712 {
-    bytes32 private _INITIAL_DOMAIN_SEPARATOR;
+library EIP712 {
+    bytes32 internal constant DOMAIN_TYPEHASH =
+        keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)");
 
-    uint256 private _INITIAL_CHAIN_ID;
+    function hashDomainSeparator(
+        string memory name,
+        string memory version,
+        address verifyingContract
+    ) internal view returns (bytes32 result) {
+        bytes32 typehash = DOMAIN_TYPEHASH;
 
-    bytes32 private _HASHED_NAME;
-
-    bytes32 private _HASHED_VERSION;
-
-    bytes32 private constant _DOMAIN_TYPEHASH = 0x8b73c3c69bb8fe3d512ecc4cf759cc79239f7b179b0ffacaa9a75d522b39400f;
-
-    constructor(string memory name, string memory version) {
-        // Hash variables using assembly for a more efficient computation
-        assembly {
-            sstore(_HASHED_NAME.slot, keccak256(add(name, 32), mload(name)))
-            sstore(_HASHED_VERSION.slot, keccak256(add(version, 32), mload(version)))
-            sstore(_INITIAL_CHAIN_ID.slot, chainid())
-        }
-
-        _INITIAL_DOMAIN_SEPARATOR = _buildDomainSeparator();
-    }
-
-    function DOMAIN_SEPARATOR() public view virtual returns (bytes32) {
-        return block.chainid == _INITIAL_CHAIN_ID ? _INITIAL_DOMAIN_SEPARATOR : _buildDomainSeparator();
-    }
-
-    function _buildDomainSeparator() private view returns (bytes32 result) {
         assembly {
             let ptr := mload(0x40)
 
-            mstore(ptr, _DOMAIN_TYPEHASH) // Domain typehash
-            mstore(add(ptr, 32), sload(_HASHED_NAME.slot)) // Hashed name
-            mstore(add(ptr, 64), sload(_HASHED_VERSION.slot)) // Hashed version
+            mstore(ptr, typehash) // Domain typehash
+            mstore(add(ptr, 32), keccak256(add(name, 32), mload(name))) // Hashed name
+            mstore(add(ptr, 64), keccak256(add(version, 32), mload(version))) // Hashed version
             mstore(add(ptr, 96), chainid()) // Chain id
-            mstore(add(ptr, 128), address()) // Verifying address
+            mstore(add(ptr, 128), verifyingContract) // Verifying address
 
             result := keccak256(ptr, 160)
         }
     }
 
-    function _hashTypedData(bytes32 digest) internal view returns (bytes32 result) {
-        bytes32 domainSeparator = DOMAIN_SEPARATOR();
+    function hashTypedData(bytes32 domainSeparator, bytes32 digest) internal pure returns (bytes32 result) {
         assembly {
             let ptr := mload(0x40)
 
