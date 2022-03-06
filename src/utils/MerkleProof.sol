ecda0aeb774a303e6ba07e9804fc7be8e2abd33b
rajivpoc <rajiv.patel.oconnor@gmail.com>
2022-03-06 17:48:50 -0500

    fix: load values from proof array

--- src/utils/MerkleProof.sol
+++ src/utils/MerkleProof.sol
@@ -3,11 +3,11 @@ pragma solidity >=0.8.0;
 
 library MerkleProof {
     function verify(
-        bytes32 root,
         bytes32[] memory proof,
+        bytes32 root,
         bytes32 leaf
     ) internal pure returns (bool isValid) {
-        assembly {
+         assembly {
             let computedHash := leaf
             let proofLength := mload(proof)
 
@@ -21,14 +21,15 @@ library MerkleProof {
             for {let end := add(data, mul(proofLength, 0x20))}
             lt(data, end)
             { data := add(data, 0x20) } {
-                switch iszero(gt(computedHash, data))
+                let loadedData := mload(data)
+                switch gt(computedHash, loadedData)
                 case 0 {
                     mstore(0x00, computedHash)
-                    mstore(0x20, data)
+                    mstore(0x20, loadedData)
                     computedHash := keccak256(0x00, 0x40)
                 }
                 default {
-                    mstore(0x00, data)
+                    mstore(0x00, loadedData)
                     mstore(0x20, computedHash)
                     computedHash := keccak256(0x00, 0x40)
                 }
