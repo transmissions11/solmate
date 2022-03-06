a1cc921d1297a260f09ef9fe035baf7b474a05a7
t11s <greenfeatherhelp@gmail.com>
2022-03-06 00:22:16 -0800

    📝 Update header

--- src/utils/SafeTransferLib.sol
+++ src/utils/SafeTransferLib.sol
@@ -5,7 +5,6 @@ import {ERC20} from "../tokens/ERC20.sol";
 
 /// @notice Safe ETH and ERC20 transfer library that gracefully handles missing return values.
 /// @author Solmate (https://github.com/Rari-Capital/solmate/blob/main/src/utils/SafeTransferLib.sol)
-/// @author Modified from Gnosis (https://github.com/gnosis/gp-v2-contracts/blob/main/src/contracts/libraries/GPv2SafeERC20.sol)
 /// @dev Use with caution! Some functions in this library knowingly create dirty bits at the destination of the free memory pointer.
 /// @dev Note that none of the functions in this library check that a token has code at all! That responsibility is delegated to the caller.
 library SafeTransferLib {
