2509441d40febae9a9e092058742864ab4d1f0e9
t11s <greenfeatherhelp@gmail.com>
2022-03-06 21:11:34 -0800

    🚧 WIP

--- src/utils/SafeTransferLib.sol
+++ src/utils/SafeTransferLib.sol
@@ -8,6 +8,8 @@ import {ERC20} from "../tokens/ERC20.sol";
 /// @dev Use with caution! Some functions in this library knowingly create dirty bits at the destination of the free memory pointer.
 /// @dev Note that none of the functions in this library check that a token has code at all! That responsibility is delegated to the caller.
 library SafeTransferLib {
+    event Debug(bool one, bool two, uint256 retsize);
+
     /*///////////////////////////////////////////////////////////////
                             ETH OPERATIONS
     //////////////////////////////////////////////////////////////*/
@@ -46,7 +48,7 @@ library SafeTransferLib {
             mstore(add(freeMemoryPointer, 68), amount) // Append the "amount" argument.
 
             // Fill up the scratch space so it's easy to tell if the call returns <32 bytes.
-            mstore(0, 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF)
+            mstore(0, 0x0000000000000000000000000000000000000000000000000000000000000000)
 
             // Call the token and store if it succeeded or not.
             // We use 100 because the calldata length is 4 + 32 * 3.
@@ -80,20 +82,15 @@ library SafeTransferLib {
             mstore(add(freeMemoryPointer, 4), to) // Append the "to" argument.
             mstore(add(freeMemoryPointer, 36), amount) // Append the "amount" argument.
 
-            // Fill up the scratch space so it's easy to tell if the call returns <32 bytes.
-            mstore(0, 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF)
-
             // Call the token and store if it succeeded or not.
             // We use 68 because the calldata length is 4 + 32 * 2.
             // We'll copy up to 32 bytes of return data into the scratch space,
             // if it returns <32 bytes at least a portion of the junk will remain.
-            success := call(gas(), token, 0, freeMemoryPointer, 68, 0, 32)
 
-            // Set success to whether the call returned 1, except if it
-            // had no return data, in which case we assume it succeeded,
-            // or if it reverted, in which case we multiply everything by
-            // 0, setting success to zero which will decode as false below.
-            success := mul(add(iszero(returndatasize()), eq(mload(0), 1)), success)
+            success := mul(
+                add(mul(eq(mload(0), 1), gt(returndatasize(), 31)), iszero(returndatasize())),
+                call(gas(), token, 0, freeMemoryPointer, 68, 0, 32)
+            )
         }
 
         require(success, "TRANSFER_FAILED");
@@ -114,9 +111,6 @@ library SafeTransferLib {
             mstore(freeMemoryPointer, 0x095ea7b300000000000000000000000000000000000000000000000000000000)
             mstore(add(freeMemoryPointer, 4), to) // Append the "to" argument.
             mstore(add(freeMemoryPointer, 36), amount) // Append the "amount" argument.
-
-            // Fill up the scratch space so it's easy to tell if the call returns <32 bytes.
-            mstore(0, 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF)
 
             // Call the token and store if it succeeded or not.
             // We use 68 because the calldata length is 4 + 32 * 2.
