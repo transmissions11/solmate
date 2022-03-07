fd74cf5514f745da11e92e59889d882451f80edd
rajivpoc <rajiv.patel.oconnor@gmail.com>
2022-03-07 16:04:37 -0500

    test: leaf and root are same no proof supplied

--- src/test/MerkleProof.t.sol
+++ src/test/MerkleProof.t.sol
@@ -4,11 +4,18 @@ pragma solidity 0.8.10;
 import {DSTestPlus} from "./utils/DSTestPlus.sol";
 
 import {MerkleProof} from "../utils/MerkleProof.sol";
+import {MerkleProofOZ} from "../utils/MerkleProof_OZ.sol";
 
 contract MerkleProofTest is DSTestPlus {
-    function testFailVerifyEmptyMerkleProofSupplied() public pure {
+    function testVerifyEmptyMerkleProofSuppliedLeafAndRootSame() public {
         bytes32[] memory proof;
-        MerkleProof.verify(proof, 0x00, 0x00);
+        assertBoolEq(MerkleProof.verify(proof, 0x00, 0x00), true);
+    }
+
+    function testVerifyEmptyMerkleProofSuppliedLeafAndRootDifferent() public {
+        bytes32[] memory proof;
+        bytes32 leaf = "a";
+        assertBoolEq(MerkleProof.verify(proof, 0x00, leaf), false);
     }
 
     function testValidProofSupplied() public {
@@ -18,7 +25,7 @@ contract MerkleProofTest is DSTestPlus {
         proof[0] = 0xb5553de315e0edf504d9150af82dafa5c4667fa618ed0a6f19c69b41166c5510;
         proof[1] = 0x0b42b6393c1f53060fe3ddbfcd7aadcca894465a5a438f69c87d790b2299b9b2;
         bytes32 root = 0x5842148bc6ebeb52af882a317c765fccd3ae80589b21a9b8cbf21abb630e46a7;
-        bytes32 leaf = 0x3ac225168df54212a25c1c01fd35bebfea408fdac2e31ddd6f80a4bbf9a5f1cb;
+        bytes32 leaf = 0x3ac225168df54212a25c1c01fd35bebfea408fdac2e31ddd6f80a4bbf9a5f1cb; 
         assertBoolEq(MerkleProof.verify(proof, root, leaf), true);
     }
 
