293c4fd8fd16b9592e04a47b69588fb7751b307a
Vertex <leaddev@anon.farm>
2022-03-20 06:03:22 +0530

    ⚡️ Optimize SafeCastLib (#165)
    Co-authored-by: t11s <greenfeatherhelp@gmail.com>

--- src/utils/SafeCastLib.sol
+++ src/utils/SafeCastLib.sol
@@ -6,55 +6,55 @@ pragma solidity >=0.8.0;
 /// @author Modified from OpenZeppelin (https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeCast.sol)
 library SafeCastLib {
     function safeCastTo248(uint256 x) internal pure returns (uint248 y) {
-        require(x <= type(uint248).max);
+        require(x < 1 << 248);
 
         y = uint248(x);
     }
 
     function safeCastTo224(uint256 x) internal pure returns (uint224 y) {
-        require(x <= type(uint224).max);
+        require(x < 1 << 224);
 
         y = uint224(x);
     }
 
     function safeCastTo192(uint256 x) internal pure returns (uint192 y) {
-        require(x <= type(uint192).max);
+        require(x < 1 << 192);
 
         y = uint192(x);
     }
 
     function safeCastTo160(uint256 x) internal pure returns (uint160 y) {
-        require(x <= type(uint160).max);
+        require(x < 1 << 160);
 
         y = uint160(x);
     }
 
     function safeCastTo128(uint256 x) internal pure returns (uint128 y) {
-        require(x <= type(uint128).max);
+        require(x < 1 << 128);
 
         y = uint128(x);
     }
 
     function safeCastTo96(uint256 x) internal pure returns (uint96 y) {
-        require(x <= type(uint96).max);
+        require(x < 1 << 96);
 
         y = uint96(x);
     }
 
     function safeCastTo64(uint256 x) internal pure returns (uint64 y) {
-        require(x <= type(uint64).max);
+        require(x < 1 << 64);
 
         y = uint64(x);
     }
 
     function safeCastTo32(uint256 x) internal pure returns (uint32 y) {
-        require(x <= type(uint32).max);
+        require(x < 1 << 32);
 
         y = uint32(x);
     }
 
     function safeCastTo8(uint256 x) internal pure returns (uint8 y) {
-        require(x <= type(uint8).max);
+        require(x < 1 << 8);
 
         y = uint8(x);
     }
