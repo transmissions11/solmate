e611e0eaed91a0c1756ed1b980a6830d23393925
t11s <greenfeatherhelp@gmail.com>
2022-04-08 18:07:27 -0700

    ♻️ New approach

--- src/test/utils/DSTestPlus.sol
+++ src/test/utils/DSTestPlus.sol
@@ -68,8 +68,8 @@ contract DSTestPlus is DSTest {
 
         if (delta > maxDelta) {
             emit log("Error: a ~= b not satisfied [uint]");
-            emit log_named_uint("  Expected", a);
-            emit log_named_uint("    Actual", b);
+            emit log_named_uint("  Expected", b);
+            emit log_named_uint("    Actual", a);
             emit log_named_uint(" Max Delta", maxDelta);
             emit log_named_uint("     Delta", delta);
             fail();
@@ -79,21 +79,18 @@ contract DSTestPlus is DSTest {
     function assertRelApproxEq(
         uint256 a,
         uint256 b,
-        uint256 maxPercentDelta
+        uint256 maxPercentDelta // An 18 decimal fixed point number.
     ) internal virtual {
-        // Should always pass. Catches case where both a and b are zero.
-        if(a == b) return;
-        uint256 delta = a > b ? a - b : b - a;
-        uint256 abs = a > b ? a : b;
+        if (b == 0) assertEq(a, b); // If the expected is 0, actual must be too.
 
-        uint256 percentDelta = (delta * 1e18) / abs;
+        uint256 percentDelta = ((a > b ? a - b : b - a) * 1e18) / b;
 
         if (percentDelta > maxPercentDelta) {
             emit log("Error: a ~= b not satisfied [uint]");
-            emit log_named_uint("    Expected", a);
-            emit log_named_uint("      Actual", b);
-            emit log_named_uint(" Max % Delta", maxPercentDelta);
-            emit log_named_uint("     % Delta", percentDelta);
+            emit log_named_uint("    Expected", b);
+            emit log_named_uint("      Actual", a);
+            emit log_named_decimal_uint(" Max % Delta", maxPercentDelta, 18);
+            emit log_named_decimal_uint("     % Delta", percentDelta, 18);
             fail();
         }
     }
@@ -119,7 +116,7 @@ contract DSTestPlus is DSTest {
         uint256 x,
         uint256 min,
         uint256 max
-    ) internal pure returns (uint256 result) {
+    ) internal returns (uint256 result) {
         require(max >= min, "MAX_LESS_THAN_MIN");
 
         uint256 size = max - min;
@@ -134,6 +131,8 @@ contract DSTestPlus is DSTest {
 
         // Account for decrementing x to make max inclusive.
         if (max == type(uint256).max && x != 0) result++;
+
+        emit log_named_uint("Bound Result", result);
     }
 
     function min3(
