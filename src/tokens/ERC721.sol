e8925449f69cecb37e51a4bc92e837d5d70ebb34
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-05-07 03:53:54 +0530

    Update ERC721.sol
    make erc165 selector return more trustless

--- src/tokens/ERC721.sol
+++ src/tokens/ERC721.sol
@@ -145,7 +145,7 @@ abstract contract ERC721 {
 
     function supportsInterface(bytes4 interfaceId) public view virtual returns (bool) {
         return
-            interfaceId == 0x01ffc9a7 || // ERC165 Interface ID for ERC165
+            interfaceId == this.supportsInterface.selector || // ERC165 Interface ID for ERC165
             interfaceId == 0x80ac58cd || // ERC165 Interface ID for ERC721
             interfaceId == 0x5b5e139f; // ERC165 Interface ID for ERC721Metadata
     }
