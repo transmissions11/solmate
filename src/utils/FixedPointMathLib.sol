92ee33074e6d733f2192180bfcc1092591313a0b
t11s <greenfeatherhelp@gmail.com>
2022-05-13 00:18:32 -0700

    📝 More cleanup

--- src/utils/FixedPointMathLib.sol
+++ src/utils/FixedPointMathLib.sol
@@ -165,17 +165,12 @@ library FixedPointMathLib {
 
     function sqrt(uint256 x) internal pure returns (uint256 z) {
         assembly {
-            z := 181 // The "correct" value is 1, but this saves a multiplication later.
+            let y := x // We start y at x, which will help us make our initial estimate.
 
-            let y := x
+            z := 181 // The "correct" value is 1, but this saves a multiplication later.
 
             // This segment is to get a reasonable initial estimate for the Babylonian method. With a bad
             // start, the correct # of bits increases ~linearly each iteration instead of ~quadratically.
-
-            // Goal is to get z*z*y within a small factor of x. More iterations will
-            // get y in a tighter range. Currently, we will have y in [256, 256*2^16).
-            // We ensure y >= 256 so that the relative difference between y and y+1 is small.
-            // That's not possible if x < 256 but we can just verify those cases exhaustively.
 
             // We check y >= 2^(k + 8) but shift right by k bits
             // each branch to ensure that if x >= 256, then y >= 256.
@@ -196,15 +191,20 @@ library FixedPointMathLib {
                 z := shl(8, z)
             }
 
+            // Goal was to get z*z*y within a small factor of x. More iterations could
+            // get y in a tighter range. Currently, we will have y in [256, 256*2^16).
+            // We ensured y >= 256 so that the relative difference between y and y+1 is small.
+            // That's not possible if x < 256 but we can just verify those cases exhaustively.
+
             // Now, z*z*y <= x < z*z*(y+1), and y <= 2^(16+8), and either y >= 256, or x < 256.
             // Correctness can be checked exhaustively for x < 256, so we assume y >= 256.
             // Then z*sqrt(y) is within sqrt(257)/sqrt(256) of sqrt(x), or about 20bps.
 
-            // For s in the range [1/256, 256], the estimate f(s) = (181/1024) * (s+1)
-            // is in the range (1/2.84 * sqrt(s), 2.84 * sqrt(s)), with largest error when s = 1
-            // and when s = 256 or 1/256. Since y is in [256, 256*2^16), let a = y/65536, so
-            // that a is in [1/256, 256). Then we can estimate sqrt(y) as
-            // sqrt(65536) * 181/1024 * (a + 1) = 181/4 * (y + 65536)/65536 = 181 * (y + 65536)/2^18
+            // For s in the range [1/256, 256], the estimate f(s) = (181/1024) * (s+1) is in the range
+            // (1/2.84 * sqrt(s), 2.84 * sqrt(s)), with largest error when s = 1 and when s = 256 or 1/256.
+
+            // Since y is in [256, 256*2^16), let a = y/65536, so that a is in [1/256, 256). Then we can estimate
+            // sqrt(y) using sqrt(65536) * 181/1024 * (a + 1) = 181/4 * (y + 65536)/65536 = 181 * (y + 65536)/2^18.
 
             // There is no overflow risk here since y < 2^136 after the first branch above.
             z := shr(18, mul(z, add(y, 65536))) // A mul() is saved from starting z at 181.
@@ -221,7 +221,7 @@ library FixedPointMathLib {
             // If x+1 is a perfect square, the Babylonian method cycles between
             // floor(sqrt(x)) and ceil(sqrt(x)). This check ensures we return floor.
             // See: https://en.wikipedia.org/wiki/Integer_square_root#Using_only_integer_division
-            // Since this case is rare, we save gas on the assignment and repeat division in the rare case.
+            // Since the ceil is rare, we save gas on the assignment and repeat division in the rare case.
             // If you don't care whether the floor or ceil square root is returned, you can remove this block.
             if lt(div(x, z), z) {
                 z := div(x, z)
