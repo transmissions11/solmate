a4251ada04bd662dbc449386c33b56adb2bea995
kphed <kpcrypto69@gmail.com>
2022-05-14 10:18:36 -0500

    Replace previous test with unit and fuzz tests

--- src/test/Bytes32AddressLib.t.sol
+++ src/test/Bytes32AddressLib.t.sol
@@ -6,11 +6,11 @@ import {DSTestPlus} from "./utils/DSTestPlus.sol";
 import {Bytes32AddressLib} from "../utils/Bytes32AddressLib.sol";
 
 contract Bytes32AddressLibTest is DSTestPlus {
+    address private testAddr = 0xfEEDFaCEcaFeBEEFfEEDFACecaFEBeeFfeEdfAce;
+    bytes32 private testAddrBytes32 = 0xfeedfacecafebeeffeedfacecafebeeffeedface000000000000000000000000;
+
     function testFillLast12Bytes() public {
-        assertEq(
-            Bytes32AddressLib.fillLast12Bytes(0xfEEDFaCEcaFeBEEFfEEDFACecaFEBeeFfeEdfAce),
-            0xfeedfacecafebeeffeedfacecafebeeffeedface000000000000000000000000
-        );
+        assertEq(Bytes32AddressLib.fillLast12Bytes(testAddr), testAddrBytes32);
     }
 
     function testFromLast20Bytes() public {
@@ -21,11 +21,13 @@ contract Bytes32AddressLibTest is DSTestPlus {
     }
 
     function testFromFirst20Bytes() public {
-        assertEq(
-            Bytes32AddressLib.fromFirst20Bytes(
-                Bytes32AddressLib.fillLast12Bytes(0xfEEDFaCEcaFeBEEFfEEDFACecaFEBeeFfeEdfAce)
-            ),
-            0xfEEDFaCEcaFeBEEFfEEDFACecaFEBeeFfeEdfAce
-        );
+        assertEq(Bytes32AddressLib.fromFirst20Bytes(testAddrBytes32), testAddr);
+    }
+
+    function testFromFirst20BytesFuzz(address addr) public {
+        bytes32 addrBytes32 = bytes32(bytes20(addr));
+
+        assertEq(Bytes32AddressLib.fillLast12Bytes(addr), addrBytes32);
+        assertEq(Bytes32AddressLib.fromFirst20Bytes(addrBytes32), addr);
     }
 }
