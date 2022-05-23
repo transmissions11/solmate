264054c8409081571464e3307a22c3299c23ee28
Vectorized <webby1111@hotmail.com>
2022-05-24 02:09:45 +0800

    ✨ LibBitmap tests and tiny optimization (#249)

---
+++ src/test/utils/mocks/MockLibBitmap.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity >=0.8.0;
+
+import {LibBitmap} from "../../../utils/LibBitmap.sol";
+
+contract MockLibBitmap {
+    using LibBitmap for LibBitmap.Bitmap;
+    LibBitmap.Bitmap bitmap;
+
+    function get(uint256 index) public view returns (bool result) {
+        result = bitmap.get(index);
+    }
+
+    function set(uint256 index) public {
+        bitmap.set(index);
+    }
+
+    function unset(uint256 index) public {
+        bitmap.unset(index);
+    }
+
+    function setTo(uint256 index, bool shouldSet) public {
+        bitmap.setTo(index, shouldSet);
+    }
+}
