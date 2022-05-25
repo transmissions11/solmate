31259074f23b180cd628b8960a3d77044f971a9b
Vectorized <webby1111@hotmail.com>
2022-05-25 07:41:25 +0000

    Minor tweaks to reduce stack operations

--- src/utils/MerkleProof.sol
+++ src/utils/MerkleProof.sol
@@ -11,30 +11,28 @@ library MerkleProof {
         bytes32 leaf
     ) internal pure returns (bool isValid) {
         assembly {
-            let computedHash := leaf
-
-            // Initialize `data` to the offset of `proof` in the calldata.
-            let data := proof.offset
+            // Left shift by 5 is equivalent to multiplying by 0x20.
+            let end := add(proof.offset, shl(5, proof.length))
 
             // Iterate over proof elements to compute root hash.
             for {
-                // Left shift by 5 is equivalent to multiplying by 0x20.
-                let end := add(data, shl(5, proof.length))
-            } lt(data, end) {
+                // Initialize `data` to the offset of `proof` in the calldata.
+                let data := proof.offset
+            } iszero(eq(data, end)) {
                 data := add(data, 0x20)
             } {
-                let loadedData := calldataload(data)
-                // Slot of `computedHash` in scratch space.
+                // Slot of `leaf` in scratch space.
                 // If the condition is true: 0x20, otherwise: 0x00.
-                let scratch := shl(5, gt(computedHash, loadedData))
+                let scratch := shl(5, gt(leaf, calldataload(data)))
 
                 // Store elements to hash contiguously in scratch space.
                 // Scratch space is 64 bytes (0x00 - 0x3f) and both elements are 32 bytes.
-                mstore(scratch, computedHash)
-                mstore(xor(scratch, 0x20), loadedData)
-                computedHash := keccak256(0x00, 0x40)
+                mstore(scratch, leaf)
+                mstore(xor(scratch, 0x20), calldataload(data))
+                // Reuse `leaf` to store the hash to reduce stack operations.
+                leaf := keccak256(0x00, 0x40)
             }
-            isValid := eq(computedHash, root)
+            isValid := eq(leaf, root)
         }
     }
 }
