d71f6fcbe6904112b7bc473e845a65b10067693f
Vectorized <webby1111@hotmail.com>
2022-05-27 02:37:04 +0000

    Add multi proof

--- src/test/MerkleProof.t.sol
+++ src/test/MerkleProof.t.sol
@@ -40,11 +40,85 @@ contract MerkleProofTest is DSTestPlus {
         assertBoolEq(this.verify(proof, root, leaf), false);
     }
 
+    function testMultiProofVerifyValidProofSupplied() public {
+        // Merkle tree created from ['a', 'b', 'c', 'd', 'e', 'f'].
+        // Leafs are ['b', 'f', 'd'].
+        bytes32 root = 0x1b404f199ea828ec5771fb30139c222d8417a82175fefad5cd42bc3a189bd8d5;
+        bytes32[] memory leafs = new bytes32[](3);
+        leafs[0] = 0xb5553de315e0edf504d9150af82dafa5c4667fa618ed0a6f19c69b41166c5510;
+        leafs[1] = 0xd1e8aeb79500496ef3dc2e57ba746a8315d048b7a664a2bf948db4fa91960483;
+        leafs[2] = 0xf1918e8562236eb17adc8502332f4c9c82bc14e19bfc0aa10ab674ff75b3d2f3;
+        bytes32[] memory proofs = new bytes32[](2);
+        proofs[0] = 0xa8982c89d80987fb9a510e25981ee9170206be21af3c8e0eb312ef1d3382e761;
+        proofs[1] = 0x7dea550f679f3caab547cbbc5ee1a4c978c8c039b572ba00af1baa6481b88360;
+        bool[] memory flags = new bool[](4);
+        flags[0] = false;
+        flags[1] = true;
+        flags[2] = false;
+        flags[3] = true;
+        assertBoolEq(this.multiProofVerify(root, leafs, proofs, flags), true);
+    }
+
+    function testMultiProofVerifyInvalidProofSupplied() public {
+        bytes32 root = 0x1b404f199ea828ec5771fb30139c222d8417a82175fefad5cd42bc3a189bd8d5;
+        bytes32[] memory leafs = new bytes32[](3);
+        leafs[0] = 0x14bcc435f49d130d189737f9762feb25c44ef5b886bef833e31a702af6be4748;
+        leafs[1] = 0xa766932420cc6e9072394bef2c036ad8972c44696fee29397bd5e2c06001f615;
+        leafs[2] = 0xea00237ef11bd9615a3b6d2629f2c6259d67b19bb94947a1bd739bae3415141c;
+        bytes32[] memory proofs = new bytes32[](0);
+        bool[] memory flags = new bool[](2);
+        flags[0] = true;
+        flags[1] = true;
+        assertBoolEq(this.multiProofVerify(root, leafs, proofs, flags), false);
+    }
+
+    function testMultiProofVerifyInvalidProofSupplied1() public {
+        bytes32 root = 0x04acaaffeb0baeb707a4247b9e27734c5af34744b6e9e05c53198814cf8e6606;
+        bytes32[] memory leafs = new bytes32[](2);
+        leafs[0] = 0x0b42b6393c1f53060fe3ddbfcd7aadcca894465a5a438f69c87d790b2299b9b2;
+        leafs[1] = 0xa8982c89d80987fb9a510e25981ee9170206be21af3c8e0eb312ef1d3382e761;
+        bytes32[] memory proofs = new bytes32[](3);
+        proofs[0] = 0x3ac225168df54212a25c1c01fd35bebfea408fdac2e31ddd6f80a4bbf9a5f1cb;
+        proofs[1] = 0x0000000000000000000000000000000000000000000000000000000000000000;
+        proofs[2] = 0x9c50d01bed947904793eeee1bec5476eba556d9e87045c48d078a31d37826595;
+        bool[] memory flags = new bool[](3);
+        flags[0] = false;
+        flags[1] = false;
+        flags[2] = false;
+        assertBoolEq(this.multiProofVerify(root, leafs, proofs, flags), false);
+    }
+
+    function testMultiProofVerifyInvalidProofSupplied2() public {
+        bytes32 root = 0x04acaaffeb0baeb707a4247b9e27734c5af34744b6e9e05c53198814cf8e6606;
+        bytes32[] memory leafs = new bytes32[](2);
+        leafs[0] = 0xa8982c89d80987fb9a510e25981ee9170206be21af3c8e0eb312ef1d3382e761;
+        leafs[1] = 0x0b42b6393c1f53060fe3ddbfcd7aadcca894465a5a438f69c87d790b2299b9b2;
+        bytes32[] memory proofs = new bytes32[](3);
+        proofs[0] = 0x3ac225168df54212a25c1c01fd35bebfea408fdac2e31ddd6f80a4bbf9a5f1cb;
+        proofs[1] = 0x0000000000000000000000000000000000000000000000000000000000000000;
+        proofs[2] = 0x9c50d01bed947904793eeee1bec5476eba556d9e87045c48d078a31d37826595;
+        bool[] memory flags = new bool[](4);
+        flags[0] = false;
+        flags[1] = false;
+        flags[2] = false;
+        flags[3] = false;
+        assertBoolEq(this.multiProofVerify(root, leafs, proofs, flags), false);
+    }
+
     function verify(
         bytes32[] calldata proof,
         bytes32 root,
         bytes32 leaf
     ) external pure returns (bool) {
         return MerkleProof.verify(proof, root, leaf);
+    }
+
+    function multiProofVerify(
+        bytes32 root,
+        bytes32[] calldata leafs,
+        bytes32[] calldata proofs,
+        bool[] calldata flags
+    ) external pure returns (bool) {
+        return MerkleProof.multiProofVerify(root, leafs, proofs, flags);
     }
 }
--- src/utils/MerkleProof.sol
+++ src/utils/MerkleProof.sol
@@ -16,23 +16,109 @@ library MerkleProof {
 
             // Iterate over proof elements to compute root hash.
             for {
-                // Initialize `data` to the offset of `proof` in the calldata.
-                let data := proof.offset
-            } iszero(eq(data, end)) {
-                data := add(data, 0x20)
+                // Initialize `offset` to the offset of `proof` in the calldata.
+                let offset := proof.offset
+            } iszero(eq(offset, end)) {
+                offset := add(offset, 0x20)
             } {
                 // Slot of `leaf` in scratch space.
                 // If the condition is true: 0x20, otherwise: 0x00.
-                let scratch := shl(5, gt(leaf, calldataload(data)))
+                let scratch := shl(5, gt(leaf, calldataload(offset)))
 
                 // Store elements to hash contiguously in scratch space.
                 // Scratch space is 64 bytes (0x00 - 0x3f) and both elements are 32 bytes.
                 mstore(scratch, leaf)
-                mstore(xor(scratch, 0x20), calldataload(data))
+                mstore(xor(scratch, 0x20), calldataload(offset))
                 // Reuse `leaf` to store the hash to reduce stack operations.
                 leaf := keccak256(0x00, 0x40)
             }
             isValid := eq(leaf, root)
+        }
+    }
+
+    function multiProofVerify(
+        bytes32 root,
+        bytes32[] calldata leafs,
+        bytes32[] calldata proofs,
+        bool[] calldata flags
+    ) internal pure returns (bool isValid) {
+        // Rebuilds the root by consuming and producing values on a queue.
+        // The queue starts with the `leafs` array, and goes into a `hashes` array.
+        // At the end of the process, the last value in the `hashes` array should
+        // be the root of the merkle tree.
+        assembly {
+            // If the number of flags is correct.
+            if eq(add(leafs.length, proofs.length), add(flags.length, 1)) {
+                // Left shift by 5 is equivalent to multiplying by 0x20.
+                let leafsEnd := add(leafs.offset, shl(5, leafs.length))
+                // These are the calldata offsets.
+                let leafsOffset := leafs.offset
+                let flagsOffset := flags.offset
+                let proofsOffset := proofs.offset
+
+                // We can use the free memory space for the queue.
+                // We don't need to allocate, since the queue is temporary.
+                let hashesBack := mload(0x40)
+                let hashesFront := hashesBack
+                // This is the end of the memory for the queue.
+                let end := add(hashesBack, shl(5, flags.length))
+
+                for {
+
+                } iszero(eq(hashesBack, end)) {
+
+                } {
+                    let a := 0
+
+                    // Pops a value from the queue into `a`.
+                    switch lt(leafsOffset, leafsEnd)
+                    case 0 {
+                        // Pop from `hashes` if there are no more leafs.
+                        a := mload(hashesFront)
+                        hashesFront := add(hashesFront, 0x20)
+                    }
+                    default {
+                        // Otherwise, pop from `leafs`.
+                        a := calldataload(leafsOffset)
+                        leafsOffset := add(leafsOffset, 0x20)
+                    }
+
+                    let b := 0
+                    switch calldataload(flagsOffset)
+                    case 0 {
+                        // Loads the next proof.
+                        b := calldataload(proofsOffset)
+                        proofsOffset := add(proofsOffset, 0x20)
+                    }
+                    default {
+                        // Pops a value from the queue into `a`.
+                        switch lt(leafsOffset, leafsEnd)
+                        case 0 {
+                            // Pop from `hashes` if there are no more leafs.
+                            b := mload(hashesFront)
+                            hashesFront := add(hashesFront, 0x20)
+                        }
+                        default {
+                            // Otherwise, pop from `leafs`.
+                            b := calldataload(leafsOffset)
+                            leafsOffset := add(leafsOffset, 0x20)
+                        }
+                    }
+
+                    flagsOffset := add(flagsOffset, 0x20)
+
+                    // Slot of `a` in scratch space.
+                    // If the condition is true: 0x20, otherwise: 0x00.
+                    let scratch := shl(5, gt(a, b))
+                    // Hash the scratch space and push the result onto the queue.
+                    mstore(scratch, a)
+                    mstore(xor(scratch, 0x20), b)
+                    mstore(hashesBack, keccak256(0x00, 0x40))
+                    hashesBack := add(hashesBack, 0x20)
+                }
+                // Checks if the last value in the queue is same as the root.
+                isValid := eq(mload(sub(end, 0x20)), root)
+            }
         }
     }
 }
