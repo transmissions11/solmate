9d262b6bebedcb5cba65778ee26edeccdcff3dfa
Vectorized <webby1111@hotmail.com>
2022-05-30 03:10:31 +0000

    Make comments nicer

--- src/utils/SafeTransferLib.sol
+++ src/utils/SafeTransferLib.sol
@@ -16,9 +16,9 @@ library SafeTransferLib {
             // Transfer the ETH and store if it succeeded or not.
             let success := call(gas(), to, amount, 0, 0, 0, 0)
             if iszero(success) {
-                mstore(0x64, 0x08c379a0) // Function selector of error method.
+                mstore(0x64, 0x08c379a0) // Function selector of the error method.
                 mstore(0x84, 0x20) // Offset of the error string.
-                mstore(0xc3, "\x13ETH_TRANSFER_FAILED") // Error string length + error string.
+                mstore(0xc3, "\x13ETH_TRANSFER_FAILED") // Error string's length and bytes.
                 revert(0x80, 0x64) // Revert with (offset, size)
             }
         }
@@ -55,9 +55,9 @@ library SafeTransferLib {
             )
 
             if iszero(success) {
-                mstore(0x64, 0x08c379a0) // Function selector of error method.
+                mstore(0x64, 0x08c379a0) // Function selector of the error method.
                 mstore(0x84, 0x20) // Offset of the error string.
-                mstore(0xc3, "\x14TRANSFER_FROM_FAILED") // Error string length + error string.
+                mstore(0xc3, "\x14TRANSFER_FROM_FAILED") // Error string's length and bytes.
                 revert(0x80, 0x64) // Revert with (offset, size)
             }
 
@@ -91,9 +91,9 @@ library SafeTransferLib {
             )
 
             if iszero(success) {
-                mstore(0x64, 0x08c379a0) // Function selector of error method.
+                mstore(0x64, 0x08c379a0) // Function selector of the error method.
                 mstore(0x84, 0x20) // Offset of the error string.
-                mstore(0xc3, "\x0fTRANSFER_FAILED") // Error string length + error string.
+                mstore(0xc3, "\x0fTRANSFER_FAILED") // Error string's length and bytes.
                 revert(0x80, 0x64) // Revert with (offset, size)
             }
 
@@ -127,9 +127,9 @@ library SafeTransferLib {
             )
 
             if iszero(success) {
-                mstore(0x64, 0x08c379a0) // Function selector of error method.
+                mstore(0x64, 0x08c379a0) // Function selector of the error method.
                 mstore(0x84, 0x20) // Offset of the error string.
-                mstore(0xc3, "\x0eAPPROVE_FAILED") // Error string length + error string.
+                mstore(0xc3, "\x0eAPPROVE_FAILED") // Error string's length and bytes.
                 revert(0x80, 0x64) // Revert with (offset, size)
             }
 
