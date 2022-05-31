d4820de6c4793266866edf779f787890190b49ac
Vectorized <webby1111@hotmail.com>
2022-05-31 08:36:13 +0000

    Edit comments

--- src/test/Base64.t.sol
+++ src/test/Base64.t.sol
@@ -47,8 +47,8 @@ contract Base64Test is DSTestPlus {
             // This ensures that the memory allocated is 32-byte aligned.
             freeMemoryPointerIs32ByteAligned := iszero(and(freeMemoryPointer, 31))
             // Write a non Base64 character to the free memory pointer.
-            // If the allocated memory is insufficient, this will write over the
-            // encoded strings and make the subsequent asserts fail.
+            // If the allocated memory is insufficient, this will change the
+            // encoded string and cause the subsequent asserts to fail.
             mstore(freeMemoryPointer, "#")
         }
         assertTrue(freeMemoryPointerIs32ByteAligned);
