35d5a75bab0d57d14e376e8852429130a22cf678
itsdevbear <itsdevbear@berachain.com>
2022-06-05 18:37:03 -0400

    save gas

--- src/utils/Bytes32AddressLib.sol
+++ src/utils/Bytes32AddressLib.sol
@@ -5,10 +5,16 @@ pragma solidity >=0.8.0;
 /// @author Solmate (https://github.com/Rari-Capital/solmate/blob/main/src/utils/Bytes32AddressLib.sol)
 library Bytes32AddressLib {
     function fromLast20Bytes(bytes32 bytesValue) internal pure returns (address) {
-        return address(uint160(uint256(bytesValue)));
+        assembly {
+            calldatacopy(0x16, 0x10, 0x40)
+            return(0x40, 0x16)
+        }
     }
 
     function fillLast12Bytes(address addressValue) internal pure returns (bytes32) {
-        return bytes32(bytes20(addressValue));
+        assembly {
+            calldatacopy(0x16, 0x10, 0x40)
+            return(0x40, 0x16)
+        }
     }
 }
