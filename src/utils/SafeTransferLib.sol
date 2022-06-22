bb57ec9437ff6bd9ffc07b9396684702c0ab5045
Vectorized <webby1111@hotmail.com>
2022-06-22 14:47:49 +0000

    Success check optimization

--- src/utils/SafeTransferLib.sol
+++ src/utils/SafeTransferLib.sol
@@ -48,7 +48,7 @@ library SafeTransferLib {
                 and(
                     // Set success to whether the call reverted, if not we check it either
                     // returned exactly 1 (can't just be non-zero data), or had no return data.
-                    or(and(eq(mload(0), 1), gt(returndatasize(), 31)), iszero(returndatasize())),
+                    or(eq(mload(0), 1), iszero(returndatasize())),
                     // We use 100 because that's the total length of our calldata (4 + 32 * 3)
                     // Counterintuitively, this call() must be positioned after the or() in the
                     // surrounding and() because and() evaluates its arguments from right to left.
@@ -85,7 +85,7 @@ library SafeTransferLib {
                 and(
                     // Set success to whether the call reverted, if not we check it either
                     // returned exactly 1 (can't just be non-zero data), or had no return data.
-                    or(and(eq(mload(0), 1), gt(returndatasize(), 31)), iszero(returndatasize())),
+                    or(eq(mload(0), 1), iszero(returndatasize())),
                     // We use 68 because that's the total length of our calldata (4 + 32 * 2)
                     // Counterintuitively, this call() must be positioned after the or() in the
                     // surrounding and() because and() evaluates its arguments from right to left.
@@ -122,7 +122,7 @@ library SafeTransferLib {
                 and(
                     // Set success to whether the call reverted, if not we check it either
                     // returned exactly 1 (can't just be non-zero data), or had no return data.
-                    or(and(eq(mload(0), 1), gt(returndatasize(), 31)), iszero(returndatasize())),
+                    or(eq(mload(0), 1), iszero(returndatasize())),
                     // We use 68 because that's the total length of our calldata (4 + 32 * 2)
                     // Counterintuitively, this call() must be positioned after the or() in the
                     // surrounding and() because and() evaluates its arguments from right to left.
