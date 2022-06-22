3d83037fe9bbbd5e2ffc94aca7e8ab5a458f5c47
Vectorized <webby1111@hotmail.com>
2022-06-22 16:07:18 +0000

    Make optimization safe

--- src/utils/SafeTransferLib.sol
+++ src/utils/SafeTransferLib.sol
@@ -39,20 +39,20 @@ library SafeTransferLib {
             let memPointer := mload(0x40)
 
             // Write the abi-encoded calldata into memory, beginning with the function selector.
-            mstore(0, 0x23b872dd00000000000000000000000000000000000000000000000000000000)
-            mstore(4, from) // Append the "from" argument.
-            mstore(36, to) // Append the "to" argument.
-            mstore(68, amount) // Append the "amount" argument.
+            mstore(0x00, 0x23b872dd)
+            mstore(0x20, from) // Append the "from" argument.
+            mstore(0x40, to) // Append the "to" argument.
+            mstore(0x60, amount) // Append the "amount" argument.
 
             if iszero(
                 and(
                     // Set success to whether the call reverted, if not we check it either
                     // returned exactly 1 (can't just be non-zero data), or had no return data.
-                    or(eq(mload(0), 1), iszero(returndatasize())),
-                    // We use 100 because that's the total length of our calldata (4 + 32 * 3)
+                    or(eq(mload(0x00), 1), iszero(returndatasize())),
+                    // We use 0x64 because that's the total length of our calldata (0x04 + 0x20 * 3)
                     // Counterintuitively, this call() must be positioned after the or() in the
                     // surrounding and() because and() evaluates its arguments from right to left.
-                    call(gas(), token, 0, 0, 100, 0, 32)
+                    call(gas(), token, 0, 0x1c, 0x64, 0x00, 0x20)
                 )
             ) {
                 mstore(0x00, "\x08\xc3\x79\xa0") // Function selector of the error method.
@@ -77,19 +77,19 @@ library SafeTransferLib {
             let memPointer := mload(0x40)
 
             // Write the abi-encoded calldata into memory, beginning with the function selector.
-            mstore(0, 0xa9059cbb00000000000000000000000000000000000000000000000000000000)
-            mstore(4, to) // Append the "to" argument.
-            mstore(36, amount) // Append the "amount" argument.
+            mstore(0x00, 0xa9059cbb)
+            mstore(0x20, to) // Append the "to" argument.
+            mstore(0x40, amount) // Append the "amount" argument.
 
             if iszero(
                 and(
                     // Set success to whether the call reverted, if not we check it either
                     // returned exactly 1 (can't just be non-zero data), or had no return data.
-                    or(eq(mload(0), 1), iszero(returndatasize())),
-                    // We use 68 because that's the total length of our calldata (4 + 32 * 2)
+                    or(eq(mload(0x00), 1), iszero(returndatasize())),
+                    // We use 0x44 because that's the total length of our calldata (0x04 + 0x20 * 2)
                     // Counterintuitively, this call() must be positioned after the or() in the
                     // surrounding and() because and() evaluates its arguments from right to left.
-                    call(gas(), token, 0, 0, 68, 0, 32)
+                    call(gas(), token, 0, 0x1c, 0x44, 0x00, 0x20)
                 )
             ) {
                 mstore(0x00, "\x08\xc3\x79\xa0") // Function selector of the error method.
@@ -99,7 +99,6 @@ library SafeTransferLib {
                 revert(0x00, 0x64) // Revert with (offset, size).
             }
 
-            mstore(0x60, 0) // Restore the zero slot to zero.
             mstore(0x40, memPointer) // Restore the memPointer.
         }
     }
@@ -114,19 +113,19 @@ library SafeTransferLib {
             let memPointer := mload(0x40)
 
             // Write the abi-encoded calldata into memory, beginning with the function selector.
-            mstore(0, 0x095ea7b300000000000000000000000000000000000000000000000000000000)
-            mstore(4, to) // Append the "to" argument.
-            mstore(36, amount) // Append the "amount" argument.
+            mstore(0x00, 0x095ea7b3)
+            mstore(0x20, to) // Append the "to" argument.
+            mstore(0x40, amount) // Append the "amount" argument.
 
             if iszero(
                 and(
                     // Set success to whether the call reverted, if not we check it either
                     // returned exactly 1 (can't just be non-zero data), or had no return data.
-                    or(eq(mload(0), 1), iszero(returndatasize())),
-                    // We use 68 because that's the total length of our calldata (4 + 32 * 2)
+                    or(eq(mload(0x00), 1), iszero(returndatasize())),
+                    // We use 0x44 because that's the total length of our calldata (0x04 + 0x20 * 2)
                     // Counterintuitively, this call() must be positioned after the or() in the
                     // surrounding and() because and() evaluates its arguments from right to left.
-                    call(gas(), token, 0, 0, 68, 0, 32)
+                    call(gas(), token, 0, 0x1c, 0x44, 0x00, 0x20)
                 )
             ) {
                 mstore(0x00, "\x08\xc3\x79\xa0") // Function selector of the error method.
@@ -136,7 +135,6 @@ library SafeTransferLib {
                 revert(0x00, 0x64) // Revert with (offset, size).
             }
 
-            mstore(0x60, 0) // Restore the zero slot to zero.
             mstore(0x40, memPointer) // Restore the memPointer.
         }
     }
