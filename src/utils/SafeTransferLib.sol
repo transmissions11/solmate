06357a4612269a025ad77b34b0faf20f12d8a492
Vectorized <webby1111@hotmail.com>
2022-07-11 20:01:42 +0000

    Change to custom errors

--- src/utils/SafeTransferLib.sol
+++ src/utils/SafeTransferLib.sol
@@ -8,6 +8,18 @@ import {ERC20} from "../tokens/ERC20.sol";
 /// @dev Caution! This library won't check that a token has code, responsibility is delegated to the caller.
 library SafeTransferLib {
     /*//////////////////////////////////////////////////////////////
+                              CUSTOM ERRORS
+    //////////////////////////////////////////////////////////////*/
+
+    error ETHTransferFailed();
+
+    error TransferFromFailed();
+
+    error TransferFailed();
+
+    error ApproveFailed();
+
+    /*//////////////////////////////////////////////////////////////
                              ETH OPERATIONS
     //////////////////////////////////////////////////////////////*/
 
@@ -15,11 +27,10 @@ library SafeTransferLib {
         assembly {
             // Transfer the ETH and check if it succeeded or not.
             if iszero(call(gas(), to, amount, 0, 0, 0, 0)) {
-                mstore(0x00, hex"08c379a0") // Function selector of the error method.
-                mstore(0x04, 0x20) // Offset of the error string.
-                mstore(0x24, 19) // Length of the error string.
-                mstore(0x44, "ETH_TRANSFER_FAILED") // The error string.
-                revert(0x00, 0x64) // Revert with (offset, size).
+                // Store the function selector of `ETHTransferFailed()`.
+                mstore(0x00, 0xb12d13eb)
+                // Revert with (offset, size).
+                revert(0x1c, 0x04)
             }
         }
     }
@@ -55,11 +66,10 @@ library SafeTransferLib {
                     call(gas(), token, 0, 0x1c, 0x64, 0x00, 0x20)
                 )
             ) {
-                mstore(0x00, hex"08c379a0") // Function selector of the error method.
-                mstore(0x04, 0x20) // Offset of the error string.
-                mstore(0x24, 20) // Length of the error string.
-                mstore(0x44, "TRANSFER_FROM_FAILED") // The error string.
-                revert(0x00, 0x64) // Revert with (offset, size).
+                // Store the function selector of `TransferFromFailed()`.
+                mstore(0x00, 0x7939f424)
+                // Revert with (offset, size).
+                revert(0x1c, 0x04)
             }
 
             mstore(0x60, 0) // Restore the zero slot to zero.
@@ -92,11 +102,10 @@ library SafeTransferLib {
                     call(gas(), token, 0, 0x1c, 0x44, 0x00, 0x20)
                 )
             ) {
-                mstore(0x00, hex"08c379a0") // Function selector of the error method.
-                mstore(0x04, 0x20) // Offset of the error string.
-                mstore(0x24, 15) // Length of the error string.
-                mstore(0x44, "TRANSFER_FAILED") // The error string.
-                revert(0x00, 0x64) // Revert with (offset, size).
+                // Store the function selector of `TransferFailed()`.
+                mstore(0x00, 0x90b8ec18)
+                // Revert with (offset, size).
+                revert(0x1c, 0x04)
             }
 
             mstore(0x40, memPointer) // Restore the memPointer.
@@ -128,11 +137,10 @@ library SafeTransferLib {
                     call(gas(), token, 0, 0x1c, 0x44, 0x00, 0x20)
                 )
             ) {
-                mstore(0x00, hex"08c379a0") // Function selector of the error method.
-                mstore(0x04, 0x20) // Offset of the error string.
-                mstore(0x24, 14) // Length of the error string.
-                mstore(0x44, "APPROVE_FAILED") // The error string.
-                revert(0x00, 0x64) // Revert with (offset, size).
+                // Store the function selector of `ApproveFailed()`.
+                mstore(0x00, 0x3e3f8f73)
+                // Revert with (offset, size).
+                revert(0x1c, 0x04)
             }
 
             mstore(0x40, memPointer) // Restore the memPointer.
