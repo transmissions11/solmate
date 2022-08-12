d1d443013164fda083bbfc07526341c9a3dbc1f3
MerlinEgalite <egalite.merlin@gmail.com>
2022-08-12 14:28:37 +0200

    ⚡️ Use modulo and add

--- src/utils/FixedPointMathLib.sol
+++ src/utils/FixedPointMathLib.sol
@@ -63,10 +63,7 @@ library FixedPointMathLib {
             // Store x * y in z for now.
             z := mul(x, y)
 
-            // First, divide z - 1 by the denominator and add 1.
-            // We allow z - 1 to underflow if z is 0, because we multiply the
-            // end result by 0 if z is zero, ensuring we return 0 if z is zero.
-            z := mul(iszero(iszero(z)), add(div(sub(z, 1), denominator), 1))
+            z := add(gt(mod(z, denominator), 0), div(z, denominator))
         }
     }
 
