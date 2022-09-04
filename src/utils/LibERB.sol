6e1b34e7fa359f974fd8030b2a6a2a2e307340e5
t11s <greenfeatherhelp@gmail.com>
2022-09-04 11:13:36 -0700

    ♻️ Terminology change

--- src/utils/LibERB.sol
+++ src/utils/LibERB.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.8.0;
 
 import {FixedPointMathLib} from "./FixedPointMathLib.sol";
 
-// TODO: Should we return the writeNumber from the read funcs?
+// TODO: Should we return the updateNumber from the read funcs?
 
 // TODO: Does a fixed size array save gas vs a dynamic or mapping?
 
@@ -13,11 +13,11 @@ import {FixedPointMathLib} from "./FixedPointMathLib.sol";
 // and only allowed grow to be called when index == populatedSlots - 1.
 
 // TODO: Technically don't need an ERBValue struct? Unless people find
-// knowing the writeNumber helpful? It also allows touching the array
+// knowing the updateNumber helpful? It also allows touching the array
 // without messing up value in grow tho. If we removed I'd like to add
 // the "can't read uninitialized" slots rule back to the read functions.
 // I think the best thing to do would be to make it a generic param, then
-// if users find it useful they can attach write number, or timestamp, etc.
+// if users find it useful they can attach update number, or timestamp, etc.
 
 /*//////////////////////////////////////////////////////////////
                                ERB
@@ -28,13 +28,13 @@ import {FixedPointMathLib} from "./FixedPointMathLib.sol";
 library LibERB {
     struct ERBValue {
         uint224 value;
-        uint32 writeNumber;
+        uint32 updateNumber;
     }
 
     // Expected invariants:
     // - availableSlots >= 1
-    // - totalWrites >= populatedSlots
-    // - totalWrites < type(uint32).max
+    // - totalUpdates >= populatedSlots
+    // - totalUpdates < type(uint32).max
     // - availableSlots >= populatedSlots
 
     function grow(
@@ -48,42 +48,42 @@ library LibERB {
         unchecked {
             for (uint256 i = availableSlots; i < newTotalAvailableSlots; i++)
                 // We already implicitly assume we'll never
-                // reach type(uint32).max many total writes.
-                self[i].writeNumber = type(uint32).max;
+                // reach type(uint32).max many total updates.
+                self[i].updateNumber = type(uint32).max;
         }
     }
 
     function write(
         ERBValue[65535] storage self,
         uint224 value,
-        uint32 totalWrites,
+        uint32 totalUpdates,
         uint16 populatedSlots,
         uint16 availableSlots // Note: This MUST be at least 1.
-    ) internal returns (uint32 newTotalWrites, uint16 newPopulatedSlots) {
+    ) internal returns (uint32 newTotalUpdates, uint16 newPopulatedSlots) {
         unchecked {
-            newPopulatedSlots = populatedSlots == 0 || totalWrites % populatedSlots == (populatedSlots - 1)
+            newPopulatedSlots = populatedSlots == 0 || totalUpdates % populatedSlots == (populatedSlots - 1)
                 ? availableSlots
                 : populatedSlots;
 
-            newTotalWrites = totalWrites + 1; // This will overflow if we reach type(uint32).max writes.
+            newTotalUpdates = totalUpdates + 1; // This will overflow if we reach type(uint32).max updates.
 
-            self[totalWrites % newPopulatedSlots] = ERBValue({writeNumber: newTotalWrites, value: value});
+            self[totalUpdates % newPopulatedSlots] = ERBValue({value: value, updateNumber: newTotalUpdates});
         }
     }
 
     function read(
         ERBValue[65535] storage self,
-        uint32 totalWrites,
+        uint32 totalUpdates,
         uint16 populatedSlots
     ) internal view returns (ERBValue memory) {
         // We use unsafeMod so that we use index 0 when populatedSlots is 0.
-        return self[FixedPointMathLib.unsafeMod(totalWrites, populatedSlots)];
+        return self[FixedPointMathLib.unsafeMod(totalUpdates, populatedSlots)];
     }
 
     function readOffset(
         ERBValue[65535] storage self,
         uint32 offset,
-        uint32 totalWrites,
+        uint32 totalUpdates,
         uint16 populatedSlots
     ) internal view returns (ERBValue memory) {
         unchecked {
@@ -91,24 +91,24 @@ library LibERB {
             require(offset <= populatedSlots, "OUT_OF_BOUNDS");
 
             // We use unsafeMod so that we use index 0 when populatedSlots is 0.
-            // We assume the invariant totalWrites >= populatedSlots is maintained.
-            return self[FixedPointMathLib.unsafeMod(totalWrites - offset, populatedSlots)];
+            // We assume the invariant totalUpdates >= populatedSlots is maintained.
+            return self[FixedPointMathLib.unsafeMod(totalUpdates - offset, populatedSlots)];
         }
     }
 
     function readWriteNumber(
         ERBValue[65535] storage self,
-        uint32 writeNumber,
-        uint32 totalWrites,
+        uint32 updateNumber,
+        uint32 totalUpdates,
         uint16 populatedSlots
     ) internal view returns (ERBValue memory) {
         // We can't read back further than our # of populated slots will allow.
-        // This will safely revert due to underflow if writeNumber > totalWrites.
-        require(totalWrites - writeNumber <= populatedSlots, "OUT_OF_BOUNDS");
+        // This will safely revert due to underflow if updateNumber > totalUpdates.
+        require(totalUpdates - updateNumber <= populatedSlots, "OUT_OF_BOUNDS");
 
         // Return early if we just wan't the current value. We use
         // unsafeMod so that we use index 0 when populatedSlots is 0.
-        return self[FixedPointMathLib.unsafeMod(writeNumber, populatedSlots)];
+        return self[FixedPointMathLib.unsafeMod(updateNumber, populatedSlots)];
     }
 }
 
@@ -120,7 +120,7 @@ library LibERB {
 /// @author Solmate (https://github.com/Rari-Capital/solmate/blob/main/src/utils/LibERB.sol)
 library LibBoxedERB {
     struct BoxedERB {
-        uint32 totalWrites;
+        uint32 totalUpdates;
         uint16 populatedSlots;
         uint16 availableSlots;
         LibERB.ERBValue[65535] erb;
@@ -135,8 +135,8 @@ library LibBoxedERB {
         self.availableSlots = 1;
 
         // We already implicitly assume we'll never
-        // reach type(uint32).max many total writes.
-        self.erb[0].writeNumber = type(uint32).max;
+        // reach type(uint32).max many total updates.
+        self.erb[0].updateNumber = type(uint32).max;
     }
 
     function grow(BoxedERB storage self, uint16 growBy) internal {
@@ -145,23 +145,27 @@ library LibBoxedERB {
 
     // This should only be called if init has been called before.
     function write(BoxedERB storage self, uint224 value) internal {
-        (self.totalWrites, self.populatedSlots) = self.erb.write(
+        (self.totalUpdates, self.populatedSlots) = self.erb.write(
             value,
-            self.totalWrites,
+            self.totalUpdates,
             self.populatedSlots,
             self.availableSlots
         );
     }
 
     function read(BoxedERB storage self) internal view returns (LibERB.ERBValue memory) {
-        return self.erb.read(self.totalWrites, self.populatedSlots);
+        return self.erb.read(self.totalUpdates, self.populatedSlots);
     }
 
     function readOffset(BoxedERB storage self, uint32 offset) internal view returns (LibERB.ERBValue memory) {
-        return self.erb.readOffset(offset, self.totalWrites, self.populatedSlots);
+        return self.erb.readOffset(offset, self.totalUpdates, self.populatedSlots);
     }
 
-    function readWriteNumber(BoxedERB storage self, uint32 writeNumber) internal view returns (LibERB.ERBValue memory) {
-        return self.erb.readWriteNumber(writeNumber, self.totalWrites, self.populatedSlots);
+    function readWriteNumber(BoxedERB storage self, uint32 updateNumber)
+        internal
+        view
+        returns (LibERB.ERBValue memory)
+    {
+        return self.erb.readWriteNumber(updateNumber, self.totalUpdates, self.populatedSlots);
     }
 }
