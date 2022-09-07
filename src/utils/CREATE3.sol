5f785122c53ea50bae668a6c078c1d41eaa26c9c
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-09-07 22:46:57 +0900

    Update CREATE3.sol
    🪡 create3

--- src/utils/CREATE3.sol
+++ src/utils/CREATE3.sol
@@ -1,82 +1,122 @@
-// SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity >=0.8.0;
-
-import {Bytes32AddressLib} from "./Bytes32AddressLib.sol";
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
 
 /// @notice Deploy to deterministic addresses without an initcode factor.
-/// @author Solmate (https://github.com/transmissions11/solmate/blob/main/src/utils/CREATE3.sol)
-/// @author Modified from 0xSequence (https://github.com/0xSequence/create3/blob/master/contracts/Create3.sol)
+/// @author Modified from Solady (https://github.com/Vectorized/solady/blob/main/src/utils/CREATE3.sol)
 library CREATE3 {
-    using Bytes32AddressLib for bytes32;
+    error DeploymentFailed();
 
-    //--------------------------------------------------------------------------------//
-    // Opcode     | Opcode + Arguments    | Description      | Stack View             //
-    //--------------------------------------------------------------------------------//
-    // 0x36       |  0x36                 | CALLDATASIZE     | size                   //
-    // 0x3d       |  0x3d                 | RETURNDATASIZE   | 0 size                 //
-    // 0x3d       |  0x3d                 | RETURNDATASIZE   | 0 0 size               //
-    // 0x37       |  0x37                 | CALLDATACOPY     |                        //
-    // 0x36       |  0x36                 | CALLDATASIZE     | size                   //
-    // 0x3d       |  0x3d                 | RETURNDATASIZE   | 0 size                 //
-    // 0x34       |  0x34                 | CALLVALUE        | value 0 size           //
-    // 0xf0       |  0xf0                 | CREATE           | newContract            //
-    //--------------------------------------------------------------------------------//
-    // Opcode     | Opcode + Arguments    | Description      | Stack View             //
-    //--------------------------------------------------------------------------------//
-    // 0x67       |  0x67XXXXXXXXXXXXXXXX | PUSH8 bytecode   | bytecode               //
-    // 0x3d       |  0x3d                 | RETURNDATASIZE   | 0 bytecode             //
-    // 0x52       |  0x52                 | MSTORE           |                        //
-    // 0x60       |  0x6008               | PUSH1 08         | 8                      //
-    // 0x60       |  0x6018               | PUSH1 18         | 24 8                   //
-    // 0xf3       |  0xf3                 | RETURN           |                        //
-    //--------------------------------------------------------------------------------//
-    bytes internal constant PROXY_BYTECODE = hex"67_36_3d_3d_37_36_3d_34_f0_3d_52_60_08_60_18_f3";
+    error InitializationFailed();
 
-    bytes32 internal constant PROXY_BYTECODE_HASH = keccak256(PROXY_BYTECODE);
+    /**
+     * -------------------------------------------------------------------+
+     * Opcode      | Mnemonic         | Stack        | Memory             |
+     * -------------------------------------------------------------------|
+     * 36          | CALLDATASIZE     | cds          |                    |
+     * 3d          | RETURNDATASIZE   | 0 cds        |                    |
+     * 3d          | RETURNDATASIZE   | 0 0          |                    |
+     * 37          | CALLDATACOPY     |              | [0..cds): calldata |
+     * 36          | CALLDATASIZE     | cds          | [0..cds): calldata |
+     * 3d          | RETURNDATASIZE   | 0 cds        | [0..cds): calldata |
+     * 34          | CALLVALUE        | value 0 cds  | [0..cds): calldata |
+     * f0          | CREATE           | newContract  | [0..cds): calldata |
+     * -------------------------------------------------------------------|
+     * Opcode      | Mnemonic         | Stack        | Memory             |
+     * -------------------------------------------------------------------|
+     * 67 bytecode | PUSH8 bytecode   | bytecode     |                    |
+     * 3d          | RETURNDATASIZE   | 0 bytecode   |                    |
+     * 52          | MSTORE           |              | [0..8): bytecode   |
+     * 60 0x08     | PUSH1 0x08       | 0x08         | [0..8): bytecode   |
+     * 60 0x18     | PUSH1 0x18       | 0x18 0x08    | [0..8): bytecode   |
+     * f3          | RETURN           |              | [0..8): bytecode   |
+     * -------------------------------------------------------------------+
+     */
+
+    uint256 private constant _PROXY_BYTECODE = 0x67363d3d37363d34f03d5260086018f3;
+
+    bytes32 private constant _PROXY_BYTECODE_HASH = 0x21c35dbe1b344a2488cf3321d6ce542f8e9f305544ff09e4993a62319a497c1f;
 
     function deploy(
         bytes32 salt,
         bytes memory creationCode,
         uint256 value
     ) internal returns (address deployed) {
-        bytes memory proxyChildBytecode = PROXY_BYTECODE;
-
-        address proxy;
         assembly {
+            // Store the `_PROXY_BYTECODE` into scratch space.
+            mstore(0x00, _PROXY_BYTECODE)
             // Deploy a new contract with our pre-made bytecode via CREATE2.
-            // We start 32 bytes into the code to avoid copying the byte length.
-            proxy := create2(0, add(proxyChildBytecode, 32), mload(proxyChildBytecode), salt)
-        }
-        require(proxy != address(0), "DEPLOYMENT_FAILED");
+            let proxy := create2(0, 0x10, 0x10, salt)
 
-        deployed = getDeployed(salt);
-        (bool success, ) = proxy.call{value: value}(creationCode);
-        require(success && deployed.code.length != 0, "INITIALIZATION_FAILED");
+            // If the result of `create2` is the zero address, revert.
+            if iszero(proxy) {
+                // Store the function selector of `DeploymentFailed()`.
+                mstore(0x00, 0x30116425)
+                // Revert with (offset, size).
+                revert(0x1c, 0x04)
+            }
+
+            // Store the proxy's address.
+            mstore(0x14, proxy)
+            // 0xd6 = 0xc0 (short RLP prefix) + 0x16 (length of: 0x94 ++ proxy ++ 0x01).
+            // 0x94 = 0x80 + 0x14 (0x14 = the length of an address, 20 bytes, in hex).
+            mstore(0x00, 0xd694)
+            // Nonce of the proxy contract (1).
+            mstore8(0x34, 0x01)
+
+            deployed := keccak256(0x1e, 0x17)
+
+            // If the `call` fails, revert.
+            if iszero(
+                call(
+                    gas(), // Gas remaining.
+                    proxy, // Proxy's address.
+                    value, // Ether value.
+                    add(creationCode, 0x20), // Start of `creationCode`.
+                    mload(creationCode), // Length of `creationCode`.
+                    0x00, // Offset of output.
+                    0x00 // Length of output.
+                )
+            ) {
+                // Store the function selector of `InitializationFailed()`.
+                mstore(0x00, 0x19b991a8)
+                // Revert with (offset, size).
+                revert(0x1c, 0x04)
+            }
+
+            // If the code size of `deployed` is zero, revert.
+            if iszero(extcodesize(deployed)) {
+                // Store the function selector of `InitializationFailed()`.
+                mstore(0x00, 0x19b991a8)
+                // Revert with (offset, size).
+                revert(0x1c, 0x04)
+            }
+        }
     }
 
-    function getDeployed(bytes32 salt) internal view returns (address) {
-        address proxy = keccak256(
-            abi.encodePacked(
-                // Prefix:
-                bytes1(0xFF),
-                // Creator:
-                address(this),
-                // Salt:
-                salt,
-                // Bytecode hash:
-                PROXY_BYTECODE_HASH
-            )
-        ).fromLast20Bytes();
+    function getDeployed(bytes32 salt) internal view returns (address deployed) {
+        assembly {
+            // Cache the free memory pointer.
+            let m := mload(0x40)
+            // Store `address(this)`.
+            mstore(0x00, address())
+            // Store the prefix.
+            mstore8(0x0b, 0xff)
+            // Store the salt.
+            mstore(0x20, salt)
+            // Store the bytecode hash.
+            mstore(0x40, _PROXY_BYTECODE_HASH)
 
-        return
-            keccak256(
-                abi.encodePacked(
-                    // 0xd6 = 0xc0 (short RLP prefix) + 0x16 (length of: 0x94 ++ proxy ++ 0x01)
-                    // 0x94 = 0x80 + 0x14 (0x14 = the length of an address, 20 bytes, in hex)
-                    hex"d6_94",
-                    proxy,
-                    hex"01" // Nonce of the proxy contract (1)
-                )
-            ).fromLast20Bytes();
+            // Store the proxy's address.
+            mstore(0x14, keccak256(0x0b, 0x55))
+            // Restore the free memory pointer.
+            mstore(0x40, m)
+            // 0xd6 = 0xc0 (short RLP prefix) + 0x16 (length of: 0x94 ++ proxy ++ 0x01).
+            // 0x94 = 0x80 + 0x14 (0x14 = the length of an address, 20 bytes, in hex).
+            mstore(0x00, 0xd694)
+            // Nonce of the proxy contract (1).
+            mstore8(0x34, 0x01)
+
+            deployed := keccak256(0x1e, 0x17)
+        }
     }
 }
