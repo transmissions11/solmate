49aa9149732475f009cc557cb1b50ce461101255
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-09-08 03:55:02 +0900

    Create LibString.sol
    strings

---
+++ src/utils/LibString.sol
@@ -0,0 +1,279 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+/// @notice Library for converting numbers into strings and other string operations.
+/// @author SolDAO (https://github.com/Sol-DAO/solmate/blob/main/src/utils/LibString.sol)
+/// @author Solady (https://github.com/vectorized/solady/blob/main/src/utils/LibString.sol)
+library LibString {
+    /*//////////////////////////////////////////////////////////////
+                              CUSTOM ERRORS
+    //////////////////////////////////////////////////////////////*/
+
+    error HexLengthInsufficient();
+
+    /*//////////////////////////////////////////////////////////////
+                           DECIMAL OPERATIONS                     
+    //////////////////////////////////////////////////////////////*/
+
+    function toString(uint256 value) internal pure returns (string memory str) {
+        assembly {
+            // The maximum value of a uint256 contains 78 digits (1 byte per digit), but
+            // we allocate 0xa0 bytes to keep the free memory pointer 32-byte word aligned.
+            // We will need 1 word for the trailing zeros padding, 1 word for the length,
+            // and 3 words for a maximum of 78 digits. Total: 5 * 0x20 = 0xa0.
+            let m := add(mload(0x40), 0xa0)
+            // Update the free memory pointer to allocate.
+            mstore(0x40, m)
+            // Assign the `str` to the end.
+            str := sub(m, 0x20)
+            // Zeroize the slot after the string.
+            mstore(str, 0)
+
+            // Cache the end of the memory to calculate the length later.
+            let end := str
+
+            // We write the string from rightmost digit to leftmost digit.
+            // The following is essentially a do-while loop that also handles the zero case.
+            // prettier-ignore
+            for { let temp := value } 1 {} {
+                str := sub(str, 1)
+                // Write the character to the pointer.
+                // The ASCII index of the '0' character is 48.
+                mstore8(str, add(48, mod(temp, 10)))
+                // Keep dividing `temp` until zero.
+                temp := div(temp, 10)
+                // prettier-ignore
+                if iszero(temp) { break }
+            }
+
+            let length := sub(end, str)
+            // Move the pointer 32 bytes leftwards to make room for the length.
+            str := sub(str, 0x20)
+            // Store the length.
+            mstore(str, length)
+        }
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                         HEXADECIMAL OPERATIONS                   
+    //////////////////////////////////////////////////////////////*/
+
+    function toHexString(uint256 value, uint256 length) internal pure returns (string memory str) {
+        assembly {
+            let start := mload(0x40)
+            // We need 0x20 bytes for the trailing zeros padding, `length * 2` bytes
+            // for the digits, 0x02 bytes for the prefix, and 0x20 bytes for the length.
+            // We add 0x20 to the total and round down to a multiple of 0x20.
+            // (0x20 + 0x20 + 0x02 + 0x20) = 0x62.
+            let m := add(start, and(add(shl(1, length), 0x62), not(0x1f)))
+            // Allocate the memory.
+            mstore(0x40, m)
+            // Assign the `str` to the end.
+            str := sub(m, 0x20)
+            // Zeroize the slot after the string.
+            mstore(str, 0)
+
+            // Cache the end to calculate the length later.
+            let end := str
+            // Store "0123456789abcdef" in scratch space.
+            mstore(0x0f, 0x30313233343536373839616263646566)
+
+            let temp := value
+            // We write the string from rightmost digit to leftmost digit.
+            // The following is essentially a do-while loop that also handles the zero case.
+            // prettier-ignore
+            for {} 1 {} {
+                str := sub(str, 2)
+                mstore8(add(str, 1), mload(and(temp, 15)))
+                mstore8(str, mload(and(shr(4, temp), 15)))
+                temp := shr(8, temp)
+                length := sub(length, 1)
+                // prettier-ignore
+                if iszero(length) { break }
+            }
+
+            if temp {
+                // Store the function selector of `HexLengthInsufficient()`.
+                mstore(0x00, 0x2194895a)
+                // Revert with (offset, size).
+                revert(0x1c, 0x04)
+            }
+
+            // Compute the string's length.
+            let strLength := add(sub(end, str), 2)
+            // Move the pointer and write the "0x" prefix.
+            str := sub(str, 0x20)
+            mstore(str, 0x3078)
+            // Move the pointer and write the length.
+            str := sub(str, 2)
+            mstore(str, strLength)
+        }
+    }
+
+    function toHexString(uint256 value) internal pure returns (string memory str) {
+        assembly {
+            let start := mload(0x40)
+            // We need 0x20 bytes for the trailing zeros padding, 0x20 bytes for the length,
+            // 0x02 bytes for the prefix, and 0x40 bytes for the digits.
+            // The next multiple of 0x20 above (0x20 + 0x20 + 0x02 + 0x40) is 0xa0.
+            let m := add(start, 0xa0)
+            // Allocate the memory.
+            mstore(0x40, m)
+            // Assign the `str` to the end.
+            str := sub(m, 0x20)
+            // Zeroize the slot after the string.
+            mstore(str, 0)
+
+            // Cache the end to calculate the length later.
+            let end := str
+            // Store "0123456789abcdef" in scratch space.
+            mstore(0x0f, 0x30313233343536373839616263646566)
+
+            // We write the string from rightmost digit to leftmost digit.
+            // The following is essentially a do-while loop that also handles the zero case.
+            // prettier-ignore
+            for { let temp := value } 1 {} {
+                str := sub(str, 2)
+                mstore8(add(str, 1), mload(and(temp, 15)))
+                mstore8(str, mload(and(shr(4, temp), 15)))
+                temp := shr(8, temp)
+                // prettier-ignore
+                if iszero(temp) { break }
+            }
+
+            // Compute the string's length.
+            let strLength := add(sub(end, str), 2)
+            // Move the pointer and write the "0x" prefix.
+            str := sub(str, 0x20)
+            mstore(str, 0x3078)
+            // Move the pointer and write the length.
+            str := sub(str, 2)
+            mstore(str, strLength)
+        }
+    }
+
+    function toHexString(address value) internal pure returns (string memory str) {
+        assembly {
+            let start := mload(0x40)
+            // We need 0x20 bytes for the length, 0x02 bytes for the prefix,
+            // and 0x28 bytes for the digits.
+            // The next multiple of 0x20 above (0x20 + 0x02 + 0x28) is 0x60.
+            str := add(start, 0x60)
+
+            // Allocate the memory.
+            mstore(0x40, str)
+            // Store "0123456789abcdef" in scratch space.
+            mstore(0x0f, 0x30313233343536373839616263646566)
+
+            let length := 20
+            // We write the string from rightmost digit to leftmost digit.
+            // The following is essentially a do-while loop that also handles the zero case.
+            // prettier-ignore
+            for { let temp := value } 1 {} {
+                str := sub(str, 2)
+                mstore8(add(str, 1), mload(and(temp, 15)))
+                mstore8(str, mload(and(shr(4, temp), 15)))
+                temp := shr(8, temp)
+                length := sub(length, 1)
+                // prettier-ignore
+                if iszero(length) { break }
+            }
+
+            // Move the pointer and write the "0x" prefix.
+            str := sub(str, 32)
+            mstore(str, 0x3078)
+            // Move the pointer and write the length.
+            str := sub(str, 2)
+            mstore(str, 42)
+        }
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                         OTHER STRING OPERATIONS                  
+    //////////////////////////////////////////////////////////////*/
+
+    function replace(
+        string memory subject,
+        string memory search,
+        string memory replacement
+    ) internal pure returns (string memory result) {
+        assembly {
+            let subjectLength := mload(subject)
+            let searchLength := mload(search)
+            let replacementLength := mload(replacement)
+
+            subject := add(subject, 0x20)
+            search := add(search, 0x20)
+            replacement := add(replacement, 0x20)
+            result := add(mload(0x40), 0x20)
+
+            let subjectEnd := add(subject, subjectLength)
+            if iszero(gt(searchLength, subjectLength)) {
+                let subjectSearchEnd := add(sub(subjectEnd, searchLength), 1)
+                let h := 0
+                if iszero(lt(searchLength, 32)) {
+                    h := keccak256(search, searchLength)
+                }
+                let m := shl(3, sub(32, and(searchLength, 31)))
+                let s := mload(search)
+                // prettier-ignore
+                for {} 1 {} {
+                    let t := mload(subject)
+                    // Whether the first `searchLength % 32` bytes of 
+                    // `subject` and `search` matches.
+                    if iszero(shr(m, xor(t, s))) {
+                        if h {
+                            if iszero(eq(keccak256(subject, searchLength), h)) {
+                                mstore(result, t)
+                                result := add(result, 1)
+                                subject := add(subject, 1)
+                                // prettier-ignore
+                                if iszero(lt(subject, subjectSearchEnd)) { break }
+                                continue
+                            }
+                        }
+                        // Copy the `replacement` one word at a time.
+                        // prettier-ignore
+                        for { let o := 0 } 1 {} {
+                            mstore(add(result, o), mload(add(replacement, o)))
+                            o := add(o, 0x20)
+                            // prettier-ignore
+                            if iszero(lt(o, replacementLength)) { break }
+                        }
+                        result := add(result, replacementLength)
+                        subject := add(subject, searchLength)    
+                        if iszero(searchLength) {
+                            mstore(result, t)
+                            result := add(result, 1)
+                            subject := add(subject, 1)
+                        }
+                        // prettier-ignore
+                        if iszero(lt(subject, subjectSearchEnd)) { break }
+                        continue
+                    }
+                    mstore(result, t)
+                    result := add(result, 1)
+                    subject := add(subject, 1)
+                    // prettier-ignore
+                    if iszero(lt(subject, subjectSearchEnd)) { break }
+                }
+            }
+
+            let resultRemainder := result
+            result := add(mload(0x40), 0x20)
+            let k := add(sub(resultRemainder, result), sub(subjectEnd, subject))
+            // Copy the rest of the string one word at a time.
+            // prettier-ignore
+            for {} lt(subject, subjectEnd) {} {
+                mstore(resultRemainder, mload(subject))
+                resultRemainder := add(resultRemainder, 0x20)
+                subject := add(subject, 0x20)
+            }
+            // Allocate memory for the length and the bytes,
+            // rounded up to a multiple of 32.
+            mstore(0x40, add(result, and(add(k, 0x40), not(0x1f))))
+            result := sub(result, 0x20)
+            mstore(result, k)
+        }
+    }
+}
