0c4d7d02765e5fa8b880cff8ab5c29d5129a3816
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-09-08 02:58:29 +0900

    Create LibBitmap.sol
    more bits

---
+++ src/utils/LibBitmap.sol
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+/// @notice Efficient bitmap library for mapping integers to single bit booleans.
+/// @author SolDAO (https://github.com/Sol-AO/solmate/blob/main/src/utils/LibBitmap.sol)
+/// @author Modified from Solady (https://github.com/vectorized/solady/blob/main/src/utils/LibBitmap.sol)
+library LibBitmap {
+    struct Bitmap {
+        mapping(uint256 => uint256) map;
+    }
+
+    function get(Bitmap storage bitmap, uint256 index) internal view returns (bool isSet) {
+        // It is better to set `isSet` to either 0 or 1, than zero vs non-zero.
+        // Both cost the same amount of gas, but the former allows the returned value
+        // to be reused without cleaning the upper bits.
+        uint256 b = (bitmap.map[index >> 8] >> (index & 0xff)) & 1;
+        assembly {
+            isSet := b
+        }
+    }
+
+    function set(Bitmap storage bitmap, uint256 index) internal {
+        bitmap.map[index >> 8] |= (1 << (index & 0xff));
+    }
+
+    function unset(Bitmap storage bitmap, uint256 index) internal {
+        bitmap.map[index >> 8] &= ~(1 << (index & 0xff));
+    }
+
+    function toggle(Bitmap storage bitmap, uint256 index) internal returns (bool newIsSet) {
+        assembly {
+            mstore(0x00, shr(8, index))
+            mstore(0x20, bitmap.slot)
+            let storageSlot := keccak256(0x00, 0x40)
+            let shift := and(index, 0xff)
+            let storageValue := sload(storageSlot)
+
+            let mask := shl(shift, 1)
+            storageValue := xor(storageValue, mask)
+            // It makes sense to return the `newIsSet`,
+            // as it allow us to skip an additional warm `sload`,
+            // and it costs minimal gas (about 15),
+            // which may be optimized away if the returned value is unused.
+            newIsSet := iszero(iszero(and(storageValue, mask)))
+            sstore(storageSlot, storageValue)
+        }
+    }
+
+    function setTo(
+        Bitmap storage bitmap,
+        uint256 index,
+        bool shouldSet
+    ) internal {
+        assembly {
+            mstore(0x20, bitmap.slot)
+            mstore(0x00, shr(8, index))
+            let storageSlot := keccak256(0x00, 0x40)
+            let storageValue := sload(storageSlot)
+            let shift := and(index, 0xff)
+
+            sstore(
+                storageSlot,
+                // Unsets the bit at `shift` via `and`, then sets its new value via `or`.
+                or(and(storageValue, not(shl(shift, 1))), shl(shift, iszero(iszero(shouldSet))))
+            )
+        }
+    }
+}
