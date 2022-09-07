3cfb90337962b5afc81a05ce529b6b9e4e0d8650
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-09-08 00:27:20 +0900

    Create ECDSA.sol
    ecdsa

---
+++ src/utils/ECDSA.sol
@@ -0,0 +1,134 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+/// @notice Gas optimized ECDSA wrapper.
+/// @author SolDAO (https://github.com/Sol-DAO/solmate/blob/main/src/utils/ECDSA.sol)
+/// @author Modified from Solady (https://github.com/vectorized/solady/blob/main/src/utils/ECDSA.sol)
+library ECDSA {
+    function recover(bytes32 hash, bytes calldata signature) internal view returns (address result) {
+        assembly {
+            if eq(signature.length, 65) {
+                // Copy the free memory pointer so that we can restore it later.
+                let m := mload(0x40)
+                // Directly copy `r` and `s` from the calldata.
+                calldatacopy(0x40, signature.offset, 0x40)
+
+                // If `s` in lower half order, such that the signature is not malleable.
+                // prettier-ignore
+                if iszero(gt(mload(0x60), 0x7fffffffffffffffffffffffffffffff5d576e7357a4501ddfe92f46681b20a0)) {
+                    mstore(0x00, hash)
+                    // Compute `v` and store it in the scratch space.
+                    mstore(0x20, byte(0, calldataload(add(signature.offset, 0x40))))
+                    pop(
+                        staticcall(
+                            gas(), // Amount of gas left for the transaction.
+                            0x01, // Address of `ecrecover`.
+                            0x00, // Start of input.
+                            0x80, // Size of input.
+                            0x40, // Start of output.
+                            0x20 // Size of output.
+                        )
+                    )
+                    // Restore the zero slot.
+                    mstore(0x60, 0)
+                    // `returndatasize()` will be `0x20` upon success, and `0x00` otherwise.
+                    result := mload(sub(0x60, returndatasize()))
+                }
+                // Restore the free memory pointer.
+                mstore(0x40, m)
+            }
+        }
+    }
+
+    function recover(
+        bytes32 hash,
+        bytes32 r,
+        bytes32 vs
+    ) internal view returns (address result) {
+        assembly {
+            // Copy the free memory pointer so that we can restore it later.
+            let m := mload(0x40)
+            // prettier-ignore
+            let s := and(vs, 0x7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff)
+
+            // If `s` in lower half order, such that the signature is not malleable.
+            // prettier-ignore
+            if iszero(gt(s, 0x7fffffffffffffffffffffffffffffff5d576e7357a4501ddfe92f46681b20a0)) {
+                mstore(0x00, hash)
+                mstore(0x20, add(shr(255, vs), 27))
+                mstore(0x40, r)
+                mstore(0x60, s)
+                pop(
+                    staticcall(
+                        gas(), // Amount of gas left for the transaction.
+                        0x01, // Address of `ecrecover`.
+                        0x00, // Start of input.
+                        0x80, // Size of input.
+                        0x40, // Start of output.
+                        0x20 // Size of output.
+                    )
+                )
+                // Restore the zero slot.
+                mstore(0x60, 0)
+                // `returndatasize()` will be `0x20` upon success, and `0x00` otherwise.
+                result := mload(sub(0x60, returndatasize()))
+            }
+            // Restore the free memory pointer.
+            mstore(0x40, m)
+        }
+    }
+
+    function toEthSignedMessageHash(bytes32 hash) internal pure returns (bytes32 result) {
+        assembly {
+            // Store into scratch space for keccak256.
+            mstore(0x20, hash)
+            mstore(0x00, "\x00\x00\x00\x00\x19Ethereum Signed Message:\n32")
+            // 0x40 - 0x04 = 0x3c
+            result := keccak256(0x04, 0x3c)
+        }
+    }
+
+    function toEthSignedMessageHash(bytes memory s) internal pure returns (bytes32 result) {
+        assembly {
+            // We need at most 128 bytes for Ethereum signed message header.
+            // The max length of the ASCII reprenstation of a uint256 is 78 bytes.
+            // The length of "\x19Ethereum Signed Message:\n" is 26 bytes (i.e. 0x1a).
+            // The next multiple of 32 above 78 + 26 is 128 (i.e. 0x80).
+
+            // Instead of allocating, we temporarily copy the 128 bytes before the
+            // start of `s` data to some variables.
+            let m3 := mload(sub(s, 0x60))
+            let m2 := mload(sub(s, 0x40))
+            let m1 := mload(sub(s, 0x20))
+            // The length of `s` is in bytes.
+            let sLength := mload(s)
+
+            let ptr := add(s, 0x20)
+
+            // `end` marks the end of the memory which we will compute the keccak256 of.
+            let end := add(ptr, sLength)
+
+            // Convert the length of the bytes to ASCII decimal representation
+            // and store it into the memory.
+            // prettier-ignore
+            for { let temp := sLength } 1 {} {
+                ptr := sub(ptr, 1)
+                mstore8(ptr, add(48, mod(temp, 10)))
+                temp := div(temp, 10)
+                // prettier-ignore
+                if iszero(temp) { break }
+            }
+
+            // Copy the header over to the memory.
+            mstore(sub(ptr, 0x20), "\x00\x00\x00\x00\x00\x00\x19Ethereum Signed Message:\n")
+            // Compute the keccak256 of the memory.
+            result := keccak256(sub(ptr, 0x1a), sub(end, sub(ptr, 0x1a)))
+
+            // Restore the previous memory.
+            mstore(s, sLength)
+            mstore(sub(s, 0x20), m1)
+            mstore(sub(s, 0x40), m2)
+            mstore(sub(s, 0x60), m3)
+        }
+    }
+}
