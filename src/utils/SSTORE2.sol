3f0981c1105b26503d34d3a6d4495cfc1e6db3f8
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-09-08 04:22:07 +0900

    Update SSTORE2.sol
    sstore

--- src/utils/SSTORE2.sol
+++ src/utils/SSTORE2.sol
@@ -1,99 +1,168 @@
-// SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity >=0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
 
 /// @notice Read and write to persistent storage at a fraction of the cost.
-/// @author Solmate (https://github.com/transmissions11/solmate/blob/main/src/utils/SSTORE2.sol)
-/// @author Modified from 0xSequence (https://github.com/0xSequence/sstore2/blob/master/contracts/SSTORE2.sol)
+/// @author SolDAO (https://github.com/Sol-DAO/solmate/blob/main/src/utils/SSTORE2.sol)
+/// @author Solady (https://github.com/vectorized/solmady/blob/main/src/utils/SSTORE2.sol)
 library SSTORE2 {
-    uint256 internal constant DATA_OFFSET = 1; // We skip the first byte as it's a STOP opcode to ensure the contract can't be called.
+    /*//////////////////////////////////////////////////////////////
+                              CUSTOM ERRORS
+    //////////////////////////////////////////////////////////////*/
+
+    error DeploymentFailed();
+
+    error InvalidPointer();
+
+    error ReadOutOfBounds();
 
     /*//////////////////////////////////////////////////////////////
-                               WRITE LOGIC
+                               WRITE LOGIC                        
     //////////////////////////////////////////////////////////////*/
 
     function write(bytes memory data) internal returns (address pointer) {
-        // Prefix the bytecode with a STOP opcode to ensure it cannot be called.
-        bytes memory runtimeCode = abi.encodePacked(hex"00", data);
-
-        bytes memory creationCode = abi.encodePacked(
-            //---------------------------------------------------------------------------------------------------------------//
-            // Opcode  | Opcode + Arguments  | Description  | Stack View                                                     //
-            //---------------------------------------------------------------------------------------------------------------//
-            // 0x60    |  0x600B             | PUSH1 11     | codeOffset                                                     //
-            // 0x59    |  0x59               | MSIZE        | 0 codeOffset                                                   //
-            // 0x81    |  0x81               | DUP2         | codeOffset 0 codeOffset                                        //
-            // 0x38    |  0x38               | CODESIZE     | codeSize codeOffset 0 codeOffset                               //
-            // 0x03    |  0x03               | SUB          | (codeSize - codeOffset) 0 codeOffset                           //
-            // 0x80    |  0x80               | DUP          | (codeSize - codeOffset) (codeSize - codeOffset) 0 codeOffset   //
-            // 0x92    |  0x92               | SWAP3        | codeOffset (codeSize - codeOffset) 0 (codeSize - codeOffset)   //
-            // 0x59    |  0x59               | MSIZE        | 0 codeOffset (codeSize - codeOffset) 0 (codeSize - codeOffset) //
-            // 0x39    |  0x39               | CODECOPY     | 0 (codeSize - codeOffset)                                      //
-            // 0xf3    |  0xf3               | RETURN       |                                                                //
-            //---------------------------------------------------------------------------------------------------------------//
-            hex"60_0B_59_81_38_03_80_92_59_39_F3", // Returns all code in the contract except for the first 11 (0B in hex) bytes.
-            runtimeCode // The bytecode we want the contract to have after deployment. Capped at 1 byte less than the code size limit.
-        );
-
+        // Note: The assembly block below does not expand the memory.
         assembly {
-            // Deploy a new contract with the generated creation code.
-            // We start 32 bytes into the code to avoid copying the byte length.
-            pointer := create(0, add(creationCode, 32), mload(creationCode))
-        }
+            let originalDataLength := mload(data)
 
-        require(pointer != address(0), "DEPLOYMENT_FAILED");
+            // Add 1 to data size since we are prefixing it with a STOP opcode.
+            let dataSize := add(originalDataLength, 1)
+
+            /**
+             * ------------------------------------------------------------------------------+
+             * Opcode      | Mnemonic        | Stack                   | Memory              |
+             * ------------------------------------------------------------------------------|
+             * 61 codeSize | PUSH2 codeSize  | codeSize                |                     |
+             * 80          | DUP1            | codeSize codeSize       |                     |
+             * 60 0xa      | PUSH1 0xa       | 0xa codeSize codeSize   |                     |
+             * 3D          | RETURNDATASIZE  | 0 0xa codeSize codeSize |                     |
+             * 39          | CODECOPY        | codeSize                | [0..codeSize): code |
+             * 3D          | RETURNDATASZIE  | 0 codeSize              | [0..codeSize): code |
+             * F3          | RETURN          |                         | [0..codeSize): code |
+             * 00          | STOP            |                         |                     |
+             * ------------------------------------------------------------------------------+
+             * @dev Prefix the bytecode with a STOP opcode to ensure it cannot be called.
+             * Also PUSH2 is used since max contract size cap is 24,576 bytes which is less than 2 ** 16.
+             */
+            mstore(
+                data,
+                or(
+                    0x61000080600a3d393df300,
+                    // Left shift `dataSize` by 64 so that it lines up with the 0000 after PUSH2.
+                    shl(0x40, dataSize)
+                )
+            )
+
+            // Deploy a new contract with the generated creation code.
+            pointer := create(0, add(data, 0x15), add(dataSize, 0xa))
+
+            // If `pointer` is zero, revert.
+            if iszero(pointer) {
+                // Store the function selector of `DeploymentFailed()`.
+                mstore(0x00, 0x30116425)
+                // Revert with (offset, size).
+                revert(0x1c, 0x04)
+            }
+
+            // Restore original length of the variable size `data`.
+            mstore(data, originalDataLength)
+        }
     }
 
     /*//////////////////////////////////////////////////////////////
-                               READ LOGIC
+                               READ LOGIC                         
     //////////////////////////////////////////////////////////////*/
 
-    function read(address pointer) internal view returns (bytes memory) {
-        return readBytecode(pointer, DATA_OFFSET, pointer.code.length - DATA_OFFSET);
+    function read(address pointer) internal view returns (bytes memory data) {
+        assembly {
+            let pointerCodesize := extcodesize(pointer)
+            if iszero(pointerCodesize) {
+                // Store the function selector of `InvalidPointer()`.
+                mstore(0x00, 0x11052bb4)
+                // Revert with (offset, size).
+                revert(0x1c, 0x04)
+            }
+            // Offset all indices by 1 to skip the STOP opcode.
+            let size := sub(pointerCodesize, 1)
+
+            // Get the pointer to the free memory and allocate
+            // enough 32-byte words for the data and the length of the data,
+            // then copy the code to the allocated memory.
+            // Masking with 0xffe0 will suffice, since contract size is less than 16 bits.
+            data := mload(0x40)
+            mstore(0x40, add(data, and(add(size, 0x3f), 0xffe0)))
+            mstore(data, size)
+            extcodecopy(pointer, add(data, 0x20), 1, size)
+        }
     }
 
-    function read(address pointer, uint256 start) internal view returns (bytes memory) {
-        start += DATA_OFFSET;
+    function read(address pointer, uint256 start) internal view returns (bytes memory data) {
+        assembly {
+            let pointerCodesize := extcodesize(pointer)
+            if iszero(pointerCodesize) {
+                // Store the function selector of `InvalidPointer()`.
+                mstore(0x00, 0x11052bb4)
+                // Revert with (offset, size).
+                revert(0x1c, 0x04)
+            }
 
-        return readBytecode(pointer, start, pointer.code.length - start);
+            // If `!(pointer.code.size > start)`, reverts.
+            // This also handles the case where `start + 1` overflows.
+            if iszero(gt(pointerCodesize, start)) {
+                // Store the function selector of `ReadOutOfBounds()`.
+                mstore(0x00, 0x84eb0dd1)
+                // Revert with (offset, size).
+                revert(0x1c, 0x04)
+            }
+            let size := sub(pointerCodesize, add(start, 1))
+
+            // Get the pointer to the free memory and allocate
+            // enough 32-byte words for the data and the length of the data,
+            // then copy the code to the allocated memory.
+            // Masking with 0xffe0 will suffice, since contract size is less than 16 bits.
+            data := mload(0x40)
+            mstore(0x40, add(data, and(add(size, 0x3f), 0xffe0)))
+            mstore(data, size)
+            extcodecopy(pointer, add(data, 0x20), add(start, 1), size)
+        }
     }
 
     function read(
         address pointer,
         uint256 start,
         uint256 end
-    ) internal view returns (bytes memory) {
-        start += DATA_OFFSET;
-        end += DATA_OFFSET;
-
-        require(pointer.code.length >= end, "OUT_OF_BOUNDS");
-
-        return readBytecode(pointer, start, end - start);
-    }
-
-    /*//////////////////////////////////////////////////////////////
-                          INTERNAL HELPER LOGIC
-    //////////////////////////////////////////////////////////////*/
-
-    function readBytecode(
-        address pointer,
-        uint256 start,
-        uint256 size
-    ) private view returns (bytes memory data) {
+    ) internal view returns (bytes memory data) {
         assembly {
-            // Get a pointer to some free memory.
+            let pointerCodesize := extcodesize(pointer)
+            if iszero(pointerCodesize) {
+                // Store the function selector of `InvalidPointer()`.
+                mstore(0x00, 0x11052bb4)
+                // Revert with (offset, size).
+                revert(0x1c, 0x04)
+            }
+
+            // If `!(pointer.code.size > end) || (start > end)`, revert.
+            // This also handles the cases where `end + 1` or `start + 1` overflow.
+            if iszero(
+                and(
+                    gt(pointerCodesize, end), // Within bounds.
+                    iszero(gt(start, end)) // Valid range.
+                )
+            ) {
+                // Store the function selector of `ReadOutOfBounds()`.
+                mstore(0x00, 0x84eb0dd1)
+                // Revert with (offset, size).
+                revert(0x1c, 0x04)
+            }
+            let size := sub(end, start)
+
+            // Get the pointer to the free memory and allocate
+            // enough 32-byte words for the data and the length of the data,
+            // then copy the code to the allocated memory.
+            // Masking with 0xffe0 will suffice, since contract size is less than 16 bits.
             data := mload(0x40)
-
-            // Update the free memory pointer to prevent overriding our data.
-            // We use and(x, not(31)) as a cheaper equivalent to sub(x, mod(x, 32)).
-            // Adding 31 to size and running the result through the logic above ensures
-            // the memory pointer remains word-aligned, following the Solidity convention.
-            mstore(0x40, add(data, and(add(add(size, 32), 31), not(31))))
-
-            // Store the size of the data in the first 32 byte chunk of free memory.
+            mstore(0x40, add(data, and(add(size, 0x3f), 0xffe0)))
             mstore(data, size)
-
-            // Copy the code into memory right after the 32 bytes we used to store the size.
-            extcodecopy(pointer, add(data, 32), start, size)
+            extcodecopy(pointer, add(data, 0x20), add(start, 1), size)
         }
     }
 }
