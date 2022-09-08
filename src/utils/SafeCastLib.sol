c7f4828d462f0dd9e62ba55895e2ce70398d3a44
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-09-08 15:50:23 +0900

    Update SafeCastLib.sol
    bump compiler / license to MIT

--- src/utils/SafeCastLib.sol
+++ src/utils/SafeCastLib.sol
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity >=0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
 
 /// @notice Safe unsigned integer casting library that reverts on overflow.
 /// @author Solmate (https://github.com/transmissions11/solmate/blob/main/src/utils/SafeCastLib.sol)
