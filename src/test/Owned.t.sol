c198803dab319fa6bad053b9fa70837d1dcea390
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-09-09 20:03:05 +0900

    Update Owned.t.sol
    compiler

--- src/test/Owned.t.sol
+++ src/test/Owned.t.sol
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity 0.8.15;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
 
 import {DSTestPlus} from "./utils/DSTestPlus.sol";
 import {MockOwned} from "./utils/mocks/MockOwned.sol";
