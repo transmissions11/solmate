4c59817dfdb09117dcc3d10d5305cdddda089576
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-09-09 19:35:41 +0900

    Update CREATE3.t.sol
    nits

--- src/test/CREATE3.t.sol
+++ src/test/CREATE3.t.sol
@@ -27,6 +27,15 @@ contract CREATE3Test is DSTestPlus {
         assertEq(deployed.decimals(), 18);
     }
 
+    function testDeployedUpperBitsSafeForPlainSolidity() public {
+        bytes32 salt = keccak256(bytes("A salt!"));
+        address deployed = CREATE3.getDeployed(salt);
+        uint256 someNumber = 123456789;
+        uint256 packed = (someNumber << 160) | uint160(deployed);
+        uint256 someNumberUnpacked = packed >> 160;
+        assertEq(someNumber, someNumberUnpacked);
+    }
+
     function testFailDoubleDeploySameBytecode() public {
         bytes32 salt = keccak256(bytes("Salty..."));
 
@@ -41,7 +50,7 @@ contract CREATE3Test is DSTestPlus {
         CREATE3.deploy(salt, type(MockAuthChild).creationCode, 0);
     }
 
-    function testDeployERC20(
+    function testFuzzDeployERC20(
         bytes32 salt,
         string calldata name,
         string calldata symbol,
@@ -58,12 +67,12 @@ contract CREATE3Test is DSTestPlus {
         assertEq(deployed.decimals(), decimals);
     }
 
-    function testFailDoubleDeploySameBytecode(bytes32 salt, bytes calldata bytecode) public {
+    function testFailFuzzDoubleDeploySameBytecode(bytes32 salt, bytes calldata bytecode) public {
         CREATE3.deploy(salt, bytecode, 0);
         CREATE3.deploy(salt, bytecode, 0);
     }
 
-    function testFailDoubleDeployDifferentBytecode(
+    function testFailFuzzDoubleDeployDifferentBytecode(
         bytes32 salt,
         bytes calldata bytecode1,
         bytes calldata bytecode2
