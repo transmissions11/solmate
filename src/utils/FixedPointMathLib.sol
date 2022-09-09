0ee185dab1f4ea852a9b48a54ba2c042a4b02a45
Vectorized <webby1111@hotmail.com>
2022-09-09 15:13:06 +0000

    Branchless opt

--- src/utils/FixedPointMathLib.sol
+++ src/utils/FixedPointMathLib.sol
@@ -172,24 +172,14 @@ library FixedPointMathLib {
             // This segment is to get a reasonable initial estimate for the Babylonian method. With a bad
             // start, the correct # of bits increases ~linearly each iteration instead of ~quadratically.
 
+            // Let y = x / 2**r, where r = lg2(x/65536).
             // We check y >= 2^(k + 8) but shift right by k bits
             // each branch to ensure that if x >= 256, then y >= 256.
-            if iszero(lt(y, 0x10000000000000000000000000000000000)) {
-                y := shr(128, y)
-                z := shl(64, z)
-            }
-            if iszero(lt(y, 0x1000000000000000000)) {
-                y := shr(64, y)
-                z := shl(32, z)
-            }
-            if iszero(lt(y, 0x10000000000)) {
-                y := shr(32, y)
-                z := shl(16, z)
-            }
-            if iszero(lt(y, 0x1000000)) {
-                y := shr(16, y)
-                z := shl(8, z)
-            }
+            let r := shl(7, gt(x, 0xffffffffffffffffffffffffffffffffff))
+            r := or(r, shl(6, gt(shr(r, x), 0xffffffffffffffffff)))
+            r := or(r, shl(5, gt(shr(r, x), 0xffffffffff)))
+            r := or(r, shl(4, gt(shr(r, x), 0xffffff)))
+            z := shl(shr(1, r), z)
 
             // Goal was to get z*z*y within a small factor of x. More iterations could
             // get y in a tighter range. Currently, we will have y in [256, 256*2^16).
@@ -207,7 +197,7 @@ library FixedPointMathLib {
             // sqrt(y) using sqrt(65536) * 181/1024 * (a + 1) = 181/4 * (y + 65536)/65536 = 181 * (y + 65536)/2^18.
 
             // There is no overflow risk here since y < 2^136 after the first branch above.
-            z := shr(18, mul(z, add(y, 65536))) // A mul() is saved from starting z at 181.
+            z := shr(18, mul(z, add(shr(r, x), 65536))) // A mul() is saved from starting z at 181.
 
             // Given the worst case multiplicative error of 2.84 above, 7 iterations should be enough.
             z := shr(1, add(z, div(x, z)))
