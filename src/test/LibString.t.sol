44fc05e24ed4e5fc387ea7dabf54dcfada7b19bb
Vectorized <webby1111@hotmail.com>
2022-10-28 00:09:34 +0000

    Update

--- src/test/LibString.t.sol
+++ src/test/LibString.t.sol
@@ -72,4 +72,42 @@ contract LibStringTest is DSTestPlus {
             keccak256(bytes("0x0000e0ca771e21bd00057f54a68c30d400000000"))
         );
     }
+
+    function testFromAddressToHexStringChecksumed() public {
+        // All caps.
+        assertEq(
+            keccak256(bytes(LibString.toHexStringChecksumed(address(0x52908400098527886E0F7030069857D2E4169EE7)))),
+            keccak256(bytes("0x52908400098527886E0F7030069857D2E4169EE7"))
+        );
+        assertEq(
+            keccak256(bytes(LibString.toHexStringChecksumed(address(0x8617E340B3D01FA5F11F306F4090FD50E238070D)))),
+            keccak256(bytes("0x8617E340B3D01FA5F11F306F4090FD50E238070D"))
+        );
+        // All lower.
+        assertEq(
+            keccak256(bytes(LibString.toHexStringChecksumed(address(0xde709f2102306220921060314715629080e2fb77)))),
+            keccak256(bytes("0xde709f2102306220921060314715629080e2fb77"))
+        );
+        assertEq(
+            keccak256(bytes(LibString.toHexStringChecksumed(address(0x27b1fdb04752bbc536007a920d24acb045561c26)))),
+            keccak256(bytes("0x27b1fdb04752bbc536007a920d24acb045561c26"))
+        );
+        // Normal.
+        assertEq(
+            keccak256(bytes(LibString.toHexStringChecksumed(address(0x5aAeb6053F3E94C9b9A09f33669435E7Ef1BeAed)))),
+            keccak256(bytes("0x5aAeb6053F3E94C9b9A09f33669435E7Ef1BeAed"))
+        );
+        assertEq(
+            keccak256(bytes(LibString.toHexStringChecksumed(address(0xfB6916095ca1df60bB79Ce92cE3Ea74c37c5d359)))),
+            keccak256(bytes("0xfB6916095ca1df60bB79Ce92cE3Ea74c37c5d359"))
+        );
+        assertEq(
+            keccak256(bytes(LibString.toHexStringChecksumed(address(0xdbF03B407c01E7cD3CBea99509d93f8DDDC8C6FB)))),
+            keccak256(bytes("0xdbF03B407c01E7cD3CBea99509d93f8DDDC8C6FB"))
+        );
+        assertEq(
+            keccak256(bytes(LibString.toHexStringChecksumed(address(0xD1220A0cf47c7B9Be7A2E6BA89F429762e7b9aDb)))),
+            keccak256(bytes("0xD1220A0cf47c7B9Be7A2E6BA89F429762e7b9aDb"))
+        );
+    }
 }
--- src/utils/LibString.sol
+++ src/utils/LibString.sol
@@ -54,18 +54,32 @@ library LibString {
     //////////////////////////////////////////////////////////////*/
 
     function toHexString(uint256 value, uint256 length) internal pure returns (string memory str) {
+        str = toHexStringNoPrefix(value, length);
+        assembly {
+            let strLength := add(mload(str), 2) // Compute the length.
+            mstore(str, 0x3078) // Write the "0x" prefix.
+            str := sub(str, 2) // Move the pointer.
+            mstore(str, strLength) // Write the length.
+        }
+    }
+
+    function toHexStringNoPrefix(uint256 value, uint256 length) internal pure returns (string memory str) {
         assembly {
             let start := mload(0x40)
-            // We need length * 2 bytes for the digits, 2 bytes for the prefix,
-            // and 32 bytes for the length. We add 32 to the total and round down
-            // to a multiple of 32. (32 + 2 + 32) = 66.
-            str := add(start, and(add(shl(1, length), 66), not(31)))
+            // We need 0x20 bytes for the trailing zeros padding, `length * 2` bytes
+            // for the digits, 0x02 bytes for the prefix, and 0x20 bytes for the length.
+            // We add 0x20 to the total and round down to a multiple of 0x20.
+            // (0x20 + 0x20 + 0x02 + 0x20) = 0x62.
+            let m := add(start, and(add(shl(1, length), 0x62), not(0x1f)))
+            // Allocate the memory.
+            mstore(0x40, m)
+            // Assign the `str` to the end.
+            str := sub(m, 0x20)
+            // Zeroize the slot after the string.
+            mstore(str, 0)
 
             // Cache the end to calculate the length later.
             let end := str
-
-            // Allocate the memory.
-            mstore(0x40, str)
             // Store "0123456789abcdef" in scratch space.
             mstore(0x0f, 0x30313233343536373839616263646566)
 
@@ -91,29 +105,39 @@ library LibString {
             }
 
             // Compute the string's length.
-            let strLength := add(sub(end, str), 2)
-            // Move the pointer and write the "0x" prefix.
-            str := sub(str, 0x20)
-            mstore(str, 0x3078)
+            let strLength := sub(end, str)
             // Move the pointer and write the length.
-            str := sub(str, 2)
+            str := sub(str, 0x20)
             mstore(str, strLength)
         }
     }
 
     function toHexString(uint256 value) internal pure returns (string memory str) {
+        str = toHexStringNoPrefix(value);
+        assembly {
+            let strLength := add(mload(str), 2) // Compute the length.
+            mstore(str, 0x3078) // Write the "0x" prefix.
+            str := sub(str, 2) // Move the pointer.
+            mstore(str, strLength) // Write the length.
+        }
+    }
+
+    function toHexStringNoPrefix(uint256 value) internal pure returns (string memory str) {
         assembly {
             let start := mload(0x40)
-            // We need 0x20 bytes for the length, 0x02 bytes for the prefix,
-            // and 0x40 bytes for the digits.
-            // The next multiple of 0x20 above (0x20 + 2 + 0x40) is 0x80.
-            str := add(start, 0x80)
+            // We need 0x20 bytes for the trailing zeros padding, 0x20 bytes for the length,
+            // 0x02 bytes for the prefix, and 0x40 bytes for the digits.
+            // The next multiple of 0x20 above (0x20 + 0x20 + 0x02 + 0x40) is 0xa0.
+            let m := add(start, 0xa0)
+            // Allocate the memory.
+            mstore(0x40, m)
+            // Assign the `str` to the end.
+            str := sub(m, 0x20)
+            // Zeroize the slot after the string.
+            mstore(str, 0)
 
             // Cache the end to calculate the length later.
             let end := str
-
-            // Allocate the memory.
-            mstore(0x40, str)
             // Store "0123456789abcdef" in scratch space.
             mstore(0x0f, 0x30313233343536373839616263646566)
 
@@ -130,49 +154,77 @@ library LibString {
             }
 
             // Compute the string's length.
-            let strLength := add(sub(end, str), 2)
-            // Move the pointer and write the "0x" prefix.
-            str := sub(str, 0x20)
-            mstore(str, 0x3078)
+            let strLength := sub(end, str)
             // Move the pointer and write the length.
-            str := sub(str, 2)
+            str := sub(str, 0x20)
             mstore(str, strLength)
         }
     }
 
-    function toHexString(address value) internal pure returns (string memory str) {
+    function toHexStringChecksumed(address value) internal pure returns (string memory str) {
+        str = toHexString(value);
         assembly {
-            let start := mload(0x40)
-            // We need 32 bytes for the length, 2 bytes for the prefix,
-            // and 40 bytes for the digits.
-            // The next multiple of 32 above (32 + 2 + 40) is 96.
-            str := add(start, 96)
+            let o := add(str, 0x22)
+            let w := not(0)
+            let mask := shl(6, div(w, 255)) // `0b10000000100000000 ...`
+            let hashed := and(keccak256(o, 40), shl(3, div(w, 15))) // `0b10001000 ... `
+            let t := shl(240, 136) // `(0b10001000 ...) << 240`
+            // prettier-ignore
+            for { let i := 0 } 1 {} {
+                mstore(add(i, i), mul(t, byte(i, hashed)))
+                i := add(i, 1)
+                // prettier-ignore
+                if eq(i, 20) { break }
+            }
+            mstore(o, xor(mload(o), shr(1, and(mload(0x00), and(mload(o), mask)))))
+            o := add(o, 0x20)
+            mstore(o, xor(mload(o), shr(1, and(mload(0x20), and(mload(o), mask)))))
+        }
+    }
+
+    function toHexString(address value) internal pure returns (string memory str) {
+        str = toHexStringNoPrefix(value);
+        assembly {
+            let strLength := add(mload(str), 2) // Compute the length.
+            mstore(str, 0x3078) // Write the "0x" prefix.
+            str := sub(str, 2) // Move the pointer.
+            mstore(str, strLength) // Write the length.
+        }
+    }
+
+    function toHexStringNoPrefix(address value) internal pure returns (string memory str) {
+        assembly {
+            str := mload(0x40)
 
             // Allocate the memory.
-            mstore(0x40, str)
+            // We need 0x20 bytes for the trailing zeros padding, 0x20 bytes for the length,
+            // 0x02 bytes for the prefix, and 0x28 bytes for the digits.
+            // The next multiple of 0x20 above (0x20 + 0x20 + 0x02 + 0x28) is 0x80.
+            mstore(0x40, add(str, 0x80))
+
             // Store "0123456789abcdef" in scratch space.
             mstore(0x0f, 0x30313233343536373839616263646566)
 
-            let length := 20
+            str := add(str, 2)
+            mstore(str, 40)
+
+            let o := add(str, 0x20)
+            mstore(add(o, 40), 0)
+
+            value := shl(96, value)
+
             // We write the string from rightmost digit to leftmost digit.
             // The following is essentially a do-while loop that also handles the zero case.
             // prettier-ignore
-            for { let temp := value } 1 {} {
-                str := sub(str, 2)
-                mstore8(add(str, 1), mload(and(temp, 15)))
-                mstore8(str, mload(and(shr(4, temp), 15)))
-                temp := shr(8, temp)
-                length := sub(length, 1)
+            for { let i := 0 } 1 {} {
+                let p := add(o, add(i, i))
+                let temp := byte(i, value)
+                mstore8(add(p, 1), mload(and(temp, 15)))
+                mstore8(p, mload(shr(4, temp)))
+                i := add(i, 1)
                 // prettier-ignore
-                if iszero(length) { break }
+                if eq(i, 20) { break }
             }
-
-            // Move the pointer and write the "0x" prefix.
-            str := sub(str, 32)
-            mstore(str, 0x3078)
-            // Move the pointer and write the length.
-            str := sub(str, 2)
-            mstore(str, 42)
         }
     }
 }
