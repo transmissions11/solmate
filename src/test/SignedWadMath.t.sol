2e27e00db68aa2cbe36f32ccd9b9043ca14554aa
t11s <greenfeatherhelp@gmail.com>
2023-08-07 10:18:06 -0700

    🐛 Fix edge case in SignedWadMath (#380)

--- src/test/SignedWadMath.t.sol
+++ src/test/SignedWadMath.t.sol
@@ -6,12 +6,7 @@ import {DSTestPlus} from "./utils/DSTestPlus.sol";
 import {wadMul, wadDiv} from "../utils/SignedWadMath.sol";
 
 contract SignedWadMathTest is DSTestPlus {
-    function testWadMul(
-        uint256 x,
-        uint256 y,
-        bool negX,
-        bool negY
-    ) public {
+    function testWadMul(uint256 x, uint256 y, bool negX, bool negY) public {
         x = bound(x, 0, 99999999999999e18);
         y = bound(x, 0, 99999999999999e18);
 
@@ -19,6 +14,20 @@ contract SignedWadMathTest is DSTestPlus {
         int256 yPrime = negY ? -int256(y) : int256(y);
 
         assertEq(wadMul(xPrime, yPrime), (xPrime * yPrime) / 1e18);
+    }
+
+    function testFailWadMulEdgeCase() public pure {
+        int256 x = -1;
+        int256 y = type(int256).min;
+
+        wadMul(x, y);
+    }
+
+    function testFailWadMulEdgeCase2() public pure {
+        int256 x = type(int256).min;
+        int256 y = -1;
+
+        wadMul(x, y);
     }
 
     function testFailWadMulOverflow(int256 x, int256 y) public pure {
@@ -30,12 +39,7 @@ contract SignedWadMathTest is DSTestPlus {
         wadMul(x, y);
     }
 
-    function testWadDiv(
-        uint256 x,
-        uint256 y,
-        bool negX,
-        bool negY
-    ) public {
+    function testWadDiv(uint256 x, uint256 y, bool negX, bool negY) public {
         x = bound(x, 0, 99999999e18);
         y = bound(x, 1, 99999999e18);
 
--- src/utils/SignedWadMath.sol
+++ src/utils/SignedWadMath.sol
@@ -58,6 +58,13 @@ function unsafeWadDiv(int256 x, int256 y) pure returns (int256 r) {
 function wadMul(int256 x, int256 y) pure returns (int256 r) {
     /// @solidity memory-safe-assembly
     assembly {
+        // Check for the specific edge case where x == -1 and y == type(int256).min
+        // For y == -1 and x == min int256, the second overflow check will catch this.
+        // See: https://secure-contracts.com/learn_evm/arithmetic-checks.html#arithmetic-checks-for-int256-multiplication
+        if and(eq(x, not(0)), eq(y, 0x8000000000000000000000000000000000000000000000000000000000000000)) {
+            revert(0, 0)
+        }
+
         // Store x * y in r for now.
         r := mul(x, y)
 
