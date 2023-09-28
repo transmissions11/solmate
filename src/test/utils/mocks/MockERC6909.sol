1e90ba48b84834547605eec0b9a2e024dc0242d5
jtriley.eth <Jtriley15@gmail.com>
2023-09-28 00:09:06 -0400

    add 6909

---
+++ src/test/utils/mocks/MockERC6909.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity >=0.8.0;
+
+import {ERC6909} from "../../../tokens/ERC6909.sol";
+
+contract MockERC6909 is ERC6909 {
+    function mint(address receiver, uint256 id, uint256 amount) public virtual {
+        _mint(receiver, id, amount);
+    }
+
+    function burn(address sender, uint256 id, uint256 amount) public virtual {
+        _burn(sender, id, amount);
+    }
+}
