6b63ece64b409e067a9506a0e1afb01f3f7c422c
t11s <greenfeatherhelp@gmail.com>
2024-02-08 17:38:27 -0800

    ♻️ pragma solidity >=0.8.24

--- src/utils/TransientReentrancyGuard.sol
+++ src/utils/TransientReentrancyGuard.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.0;
+pragma solidity >=0.8.24;
 
 /// @notice Gas optimized reentrancy protection for smart contracts. Leverages Cancun transient storage.
 /// @author Solmate (https://github.com/transmissions11/solmate/blob/main/src/utils/TransientReentrancyGuard.sol)
