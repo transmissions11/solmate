e29bde5d9e91b25bad226802d44f80023d1b8fb6
MiloTruck <milotruckctf@gmail.com>
2024-10-18 17:53:15 +0800

    Add short circuit to optimize gas for regular ERC20s

--- src/utils/SafeTransferLib.sol
+++ src/utils/SafeTransferLib.sol
@@ -46,19 +46,15 @@ library SafeTransferLib {
             mstore(add(freeMemoryPointer, 36), and(to, 0xffffffffffffffffffffffffffffffffffffffff)) // Append and mask the "to" argument.
             mstore(add(freeMemoryPointer, 68), amount) // Append the "amount" argument. Masking not required as it's a full 32 byte type.
 
-            success := and(
-                // Set success to whether the call reverted, if not we check it either
-                // returned exactly 1 (can't just be non-zero data), or had no return data and token has code.
-                or(
-                    and(eq(mload(0), 1), gt(returndatasize(), 31)),
-                    iszero(or(iszero(extcodesize(token)), returndatasize()))
-                ),
-                // We use 100 because the length of our calldata totals up like so: 4 + 32 * 3.
-                // We use 0 and 32 to copy up to 32 bytes of return data into the scratch space.
-                // Counterintuitively, this call must be positioned second to the or() call in the
-                // surrounding and() call or else returndatasize() will be zero during the computation.
-                call(gas(), token, 0, freeMemoryPointer, 100, 0, 32)
-            )
+            // We use 100 because the length of our calldata totals up like so: 4 + 32 * 3.
+            // We use 0 and 32 to copy up to 32 bytes of return data into the scratch space.
+            success := call(gas(), token, 0, freeMemoryPointer, 100, 0, 32)
+
+            // Set success to whether the call reverted, if not we check it either
+            // returned exactly 1 (can't just be non-zero data), or had no return data and token has code.
+            if and(iszero(and(eq(mload(0), 1), gt(returndatasize(), 31))), success) {
+                success := iszero(or(iszero(extcodesize(token)), returndatasize())) 
+            }
         }
 
         require(success, "TRANSFER_FROM_FAILED");
@@ -81,19 +77,15 @@ library SafeTransferLib {
             mstore(add(freeMemoryPointer, 4), and(to, 0xffffffffffffffffffffffffffffffffffffffff)) // Append and mask the "to" argument.
             mstore(add(freeMemoryPointer, 36), amount) // Append the "amount" argument. Masking not required as it's a full 32 byte type.
 
-            success := and(
-                // Set success to whether the call reverted, if not we check it either
-                // returned exactly 1 (can't just be non-zero data), or had no return data and token has code.
-                or(
-                    and(eq(mload(0), 1), gt(returndatasize(), 31)),
-                    iszero(or(iszero(extcodesize(token)), returndatasize()))
-                ),
-                // We use 68 because the length of our calldata totals up like so: 4 + 32 * 2.
-                // We use 0 and 32 to copy up to 32 bytes of return data into the scratch space.
-                // Counterintuitively, this call must be positioned second to the or() call in the
-                // surrounding and() call or else returndatasize() will be zero during the computation.
-                call(gas(), token, 0, freeMemoryPointer, 68, 0, 32)
-            )
+            // We use 68 because the length of our calldata totals up like so: 4 + 32 * 2.
+            // We use 0 and 32 to copy up to 32 bytes of return data into the scratch space.
+            success := call(gas(), token, 0, freeMemoryPointer, 68, 0, 32)
+
+            // Set success to whether the call reverted, if not we check it either
+            // returned exactly 1 (can't just be non-zero data), or had no return data and token has code.
+            if and(iszero(and(eq(mload(0), 1), gt(returndatasize(), 31))), success) {
+                success := iszero(or(iszero(extcodesize(token)), returndatasize())) 
+            }
         }
 
         require(success, "TRANSFER_FAILED");
@@ -116,19 +108,15 @@ library SafeTransferLib {
             mstore(add(freeMemoryPointer, 4), and(to, 0xffffffffffffffffffffffffffffffffffffffff)) // Append and mask the "to" argument.
             mstore(add(freeMemoryPointer, 36), amount) // Append the "amount" argument. Masking not required as it's a full 32 byte type.
 
-            success := and(
-                // Set success to whether the call reverted, if not we check it either
-                // returned exactly 1 (can't just be non-zero data), or had no return data and token has code.
-                or(
-                    and(eq(mload(0), 1), gt(returndatasize(), 31)),
-                    iszero(or(iszero(extcodesize(token)), returndatasize()))
-                ),
-                // We use 68 because the length of our calldata totals up like so: 4 + 32 * 2.
-                // We use 0 and 32 to copy up to 32 bytes of return data into the scratch space.
-                // Counterintuitively, this call must be positioned second to the or() call in the
-                // surrounding and() call or else returndatasize() will be zero during the computation.
-                call(gas(), token, 0, freeMemoryPointer, 68, 0, 32)
-            )
+            // We use 68 because the length of our calldata totals up like so: 4 + 32 * 2.
+            // We use 0 and 32 to copy up to 32 bytes of return data into the scratch space.
+            success := call(gas(), token, 0, freeMemoryPointer, 68, 0, 32)
+
+            // Set success to whether the call reverted, if not we check it either
+            // returned exactly 1 (can't just be non-zero data), or had no return data and token has code.
+            if and(iszero(and(eq(mload(0), 1), gt(returndatasize(), 31))), success) {
+                success := iszero(or(iszero(extcodesize(token)), returndatasize())) 
+            }
         }
 
         require(success, "APPROVE_FAILED");
